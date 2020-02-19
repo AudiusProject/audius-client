d1ce8eca24e6f1d0ab12d48a0bd66a32378dbf20
Michael Piazza <michael.piazza.mp@gmail.com>
2020-02-19 11:32:53 -0800

    WIP breaking things

---
+++ packages/bedtime/typings/react/react.d.ts
@@ -0,0 +1,28 @@
+import {
+  AnyComponent,
+  ComponentChild,
+  createElement,
+  FunctionalComponent,
+  RenderableProps
+} from 'preact'
+
+export {
+  Attributes,
+  FunctionalComponent as SFC,
+  AnyComponent as ComponentType,
+  AnyComponent as JSXElementConstructor,
+  Component as ComponentClass,
+  ClassAttributes,
+  PreactContext as Context,
+  PreactProvider as Provider,
+  VNode as ReactElement,
+  VNode as ReactNode,
+  createElement,
+  Fragment,
+  Ref,
+  render,
+  JSX,
+  RenderableProps as ComponentPropsWithRef
+} from 'preact'
+
+// export type JSXElementConstructor = AnyComponent;
