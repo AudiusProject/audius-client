a5a9465f15c06b456174cf465f9217f74ea909a0
vicky guan <vicky@audius.co>
2020-08-13 10:46:09 -0700

    feed smoke test!

---
+++ packages/probers/src/__tests__/smoke/feed.js
@@ -0,0 +1,26 @@
+import getConfig from '../../config'
+import { newPage, resetBrowser, waitForNetworkIdle2 } from '../../utils'
+import { createSignedInAccountIfNecessary } from '../../flows/create-account-if-necessary'
+
+const config = getConfig()
+const timeout = config.defaultTestTimeout
+
+describe('Smoke test -- feed page', () => {
+    let page
+    let account
+
+    beforeAll(async () => {
+        page = await newPage()
+        await resetBrowser(page, config.baseUrl)
+        account = await createSignedInAccountIfNecessary(page, config.baseUrl)
+    }, timeout)
+
+    afterAll(async () => {
+        await page.close()
+    })
+
+    it('should load feed page when visited', async () => {
+        await waitForNetworkIdle2(page, page.goto(`${config.baseUrl}/feed`))
+        await page.waitForXPath("//h1[contains(text(), 'Feed')]")        
+    }, timeout)
+}, timeout)
