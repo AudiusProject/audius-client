82ba79176c62194e1d932449c096576ff799bd2f
vicky guan <vicky@audius.co>
2020-08-13 17:47:06 -0700

    add track smoke test

---
+++ packages/probers/src/__tests__/smoke/track.js
@@ -0,0 +1,37 @@
+import getConfig from '../../config'
+import { newPage, resetBrowser, waitForNetworkIdle2 } from '../../utils'
+
+const config = getConfig()
+const testTimeout = config.defaultTestTimeout
+const actionTimeout = config.tenSeconds
+
+describe('Smoke test -- track page', () => {
+  let page
+
+  beforeAll(async () => {
+    page = await newPage()
+    await resetBrowser(page, `${config.baseUrl}`)
+  }, testTimeout)
+
+  afterAll(async () => {
+    await page.close()
+  })
+
+  it('should load track page when visited', async () => {
+    // // Go to trending page
+    await waitForNetworkIdle2(page, page.goto(`${config.baseUrl}/trending`))
+
+    // Wait for trending page to load
+    await page.waitForSelector(`span[class^=TrackTile_title]`, { timeout: actionTimeout })
+
+    // Click the first track tile to go to a track page
+    await Promise.all([
+      page.click(`span[class^=TrackTile_title]`), // Redirects to track page
+      page.waitForNavigation() // Resolves after navigations has finished
+    ])
+
+    // Verify track page loaded
+    await page.waitForXPath("//button[contains(@class, 'playButton')]", { timeout: actionTimeout })
+    await page.waitForXPath("//div[starts-with(@class, 'GiantTrackTile')]", { timeout: actionTimeout })
+  }, testTimeout)
+}, testTimeout)
