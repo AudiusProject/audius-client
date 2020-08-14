4810a3a0cbed59737653b9fe9c4673458b19a4a9
vicky guan <vicky@audius.co>
2020-08-13 17:47:23 -0700

    fix indents

--- packages/probers/src/__tests__/smoke/feed.js
+++ packages/probers/src/__tests__/smoke/feed.js
@@ -6,20 +6,20 @@ const config = getConfig()
 const timeout = config.defaultTestTimeout
 
 describe('Smoke test -- feed page', () => {
-    let page
+  let page
 
-    beforeAll(async () => {
-        page = await newPage()
-        await resetBrowser(page, config.baseUrl)
-        await createSignedInAccountIfNecessary(page, config.baseUrl)
-    }, timeout)
+  beforeAll(async () => {
+    page = await newPage()
+    await resetBrowser(page, config.baseUrl)
+    await createSignedInAccountIfNecessary(page, config.baseUrl)
+  }, timeout)
 
-    afterAll(async () => {
-        await page.close()
-    })
+  afterAll(async () => {
+    await page.close()
+  })
 
-    it('should load feed page when visited', async () => {
-        await waitForNetworkIdle2(page, page.goto(`${config.baseUrl}/feed`))
-        await page.waitForXPath("//h1[contains(text(), 'Feed')]")        
-    }, timeout)
+  it('should load feed page when visited', async () => {
+    await waitForNetworkIdle2(page, page.goto(`${config.baseUrl}/feed`))
+    await page.waitForXPath("//h1[contains(text(), 'Feed')]")
+  }, timeout)
 }, timeout)
