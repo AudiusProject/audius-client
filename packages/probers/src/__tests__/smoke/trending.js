1b7c12685b2c4b37d3ea45d525d884664aa858f5
vicky guan <vicky@audius.co>
2020-08-14 12:21:39 -0700

    add 404+error check for trending and comments

--- packages/probers/src/__tests__/smoke/trending.js
+++ packages/probers/src/__tests__/smoke/trending.js
@@ -17,7 +17,13 @@ describe('Smoke test -- trending page', () => {
   })
 
   it('should load trending page when visited', async () => {
+    // Go to trending page
     await waitForNetworkIdle2(page, page.goto(`${config.baseUrl}/trending`))
+
+    // Verify that page url is not 404 nor error
+    expect(page.url()).not.toMatch(/(error|404)/)
+
+    // Verify that 'Trending' label is present on page
     await page.waitForXPath("//h1[contains(text(), 'Trending')]")
   }, timeout)
 }, timeout)
