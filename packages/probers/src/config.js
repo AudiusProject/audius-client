8edb5871fa678bfcb45230ba61a070f35ffdacd0
vicky :) <60366641+vicky-g@users.noreply.github.com>
2020-08-17 15:59:53 -0700

    Merge pull request #18 from AudiusProject/vg-smoke-tests-2
    smoke tests for album, remix, remixes, and upload

--- packages/probers/src/config.js
+++ packages/probers/src/config.js
@@ -6,7 +6,10 @@ const genericConfig = {
   fiveSeconds: 5 /* sec */ * 1000 /* ms */,
   tenSeconds: 10 /* sec */ * 1000 /* ms */,
   playlistRoute: 'df/playlist/probers_playlist_do_not_delete-511',
-  trackRoute: 'df/probers_track_do_not_delete-2851'
+  trackRoute: 'df/probers_track_do_not_delete-2851',
+  albumRoute: 'df/album/probers_album_do_not_delete-512',
+  remixRoute: 'df/probers_remix_do_not_delete-2859',
+  remixesRoute: 'chrisyu321/happy-buddy-987/remixes'
 }
 
 export const config = {
