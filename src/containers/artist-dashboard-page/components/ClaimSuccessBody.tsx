5a97047310f38afe6ea7bf835e0969d938c09e69
jowlee <joeylee0925@gmail.com>
2020-10-15 18:16:26 -0400

    Add display audio component

--- src/containers/artist-dashboard-page/components/ClaimSuccessBody.tsx
+++ src/containers/artist-dashboard-page/components/ClaimSuccessBody.tsx
@@ -1,20 +1,21 @@
-import BN from 'bn.js'
 import TwitterButton from 'components/general/TwitterButton'
 import React from 'react'
+import { BNWei } from 'store/wallet/slice'
 import { ModalBodyWrapper } from '../WalletModal'
+import DisplayAudio from './DisplayAudio'
 
 const messages = {
   newBalance: 'Your $AUDIO balance is now:',
   subtitle:
     'You now own a small part of Audius. You can earn more just by using the platform and contributing to its success!'
 }
-type ClaimSuccessBodyProps = { balance: BN }
+type ClaimSuccessBodyProps = { balance: BNWei }
 
 const ClaimSuccessBody = ({ balance }: ClaimSuccessBodyProps) => {
   return (
     <ModalBodyWrapper>
       {messages.newBalance}
-      {`${balance.toString()} $AUDIO`}
+      <DisplayAudio amount={balance} />
       {messages.subtitle}
       <TwitterButton />
     </ModalBodyWrapper>
