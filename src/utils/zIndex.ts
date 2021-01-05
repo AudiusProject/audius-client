612deb8012e35ba4aa8ed8011ca942f19c104907
jowlee <joeylee0925@gmail.com>
2021-01-05 14:43:23 -0500

    Add edit playlist container

---
+++ src/utils/zIndex.ts
@@ -0,0 +1,12 @@
+/**
+ * Standardize the use of zIndex across the application
+ *
+ * Reference: https://www.notion.so/audiusproject/Client-Modal-Design-cc9d722f2aed4a0abf14fb44f97c246e
+ */
+
+export enum zIndex {
+  EDIT_TRACK_MODAL = 1000,
+  EDIT_PLAYLIST_MODAL = 1000
+}
+
+export default zIndex
