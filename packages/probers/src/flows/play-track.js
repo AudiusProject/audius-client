7d6e451f6b9aee1294898ac3f61767492e736e86
Joseph Lee <joeylee0925@gmail.com>
2021-02-10 18:15:45 -0500

    Merge pull request #23 from AudiusProject/jowlee-wait-track-render-play
    Wait for track to render

--- packages/probers/src/flows/play-track.js
+++ packages/probers/src/flows/play-track.js
@@ -2,6 +2,10 @@ import { wait, waitForNetworkIdle2 } from '../utils'
 
 export const playTrack = async (page, baseUrl, route) => {
   await waitForNetworkIdle2(page, page.goto(`${baseUrl}/${route}`))
+
+  // Tracks may still be loading, so wait for track to render
+  await page.waitForSelector(`span[class^=TrackTile_title]`)
+
   // Play the first track tile.
   await page.click(`div[class^=TrackTile_container]`)
   // Make sure audio playback starts.
