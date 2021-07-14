e7aeacc850fb1b706a93e8fba9ed858f363bd375
sliptype <sliptype@gmail.com>
2021-07-14 15:31:51 -0500

    Increase height of popup and disable click outside modal

--- src/containers/share-sound-to-tiktok-modal/ShareSoundToTikTokModal.tsx
+++ src/containers/share-sound-to-tiktok-modal/ShareSoundToTikTokModal.tsx
@@ -126,6 +126,7 @@ const ShareSoundToTikTokModal = () => {
 
   return (
     <Modal
+      dismissOnClickOutside={status !== Status.SHARE_STARTED}
       isOpen={isOpen}
       showTitleHeader
       showDismissButton
