4036eca4509bd33565c73d97be4d1cb32e96e017
Saliou Diallo <saliou@audius.co>
2021-07-29 16:56:03 -0400

    Remove unused button

--- src/containers/audio-rewards-page/AudioRewardsPage.tsx
+++ src/containers/audio-rewards-page/AudioRewardsPage.tsx
@@ -21,7 +21,6 @@ import RewardsTile from './RewardsTile'
 import Tiers from './Tiers'
 import { BalanceTile, WalletTile } from './Tiles'
 import WalletModal from './WalletModal'
-import ClaimRewardButton from './components/ClaimRewardButton'
 import ExplainerTile from './components/ExplainerTile'
 
 export const messages = {
@@ -34,7 +33,6 @@ export const RewardsContent = () => {
   useRequiresAccount(TRENDING_PAGE)
   return (
     <>
-      <ClaimRewardButton />
       <ExplainerTile className={wm(styles.explainerTile)} />
       <WalletModal />
       <div className={wm(styles.cryptoContentContainer)}>
