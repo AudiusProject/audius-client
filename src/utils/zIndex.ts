abf848c150d9034ed98bbb7d1cd80088af92e1f2
sliptype <sliptype@gmail.com>
2021-08-03 20:13:54 -0500

    Increase z-index to account for edit playlist/track modals

--- src/utils/zIndex.ts
+++ src/utils/zIndex.ts
@@ -12,11 +12,11 @@ export enum zIndex {
   // NAVIGATOR = 14,
   NAVIGATOR_POPUP = 15,
   NAVIGATOR_POPUP_OVERFLOW_POPUP = 16,
-  IMAGE_SELECTION_POPUP = 17,
 
   // Set to 1000 to account for nested modals inside, which take a higher z-index
   EDIT_TRACK_MODAL = 1000,
-  EDIT_PLAYLIST_MODAL = 1000
+  EDIT_PLAYLIST_MODAL = 1000,
+  IMAGE_SELECTION_POPUP = 1001
 }
 
 export default zIndex
