23bb0250b5ba9dbd2c84050575a195d4c5db016c
Marcus Pasell <rickyrombo@gmail.com>
2021-10-20 19:51:07 -0700

    Fixes from bad merge

--- src/containers/feed-page/store/sagas.ts
+++ src/containers/feed-page/store/sagas.ts
@@ -1,11 +1,11 @@
-import { ID } from 'common/models/Identifiers.js'
 import { call, put, take, fork, takeEvery } from 'redux-saga/effects'
-import { FollowSource } from 'services/analytics.js'
 
+import { ID } from 'common/models/Identifiers'
 import { fetchUsers } from 'common/store/cache/users/sagas'
 import * as discoverActions from 'containers/feed-page/store/actions'
 import { feedActions } from 'containers/feed-page/store/lineups/feed/actions'
 import { fetchSuggestedFollowUserIds } from 'containers/sign-on/store/sagas'
+import { FollowSource } from 'services/analytics'
 import { waitForBackendSetup } from 'store/backend/sagas'
 import * as socialActions from 'store/social/users/actions'
 
