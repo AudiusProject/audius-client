95ae915ffca759aaa434d55b359ea72d6fef1ea5
Raymond Jacobson <ray@audius.co>
2021-10-25 16:11:01 -0700

    Add eslint no-unresolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -99,6 +99,7 @@ module.exports = {
     'space-before-function-paren': 'off',
     'generator-star-spacing': 'off',
 
+    'import/no-unresolved': 'error',
     'import/order': [
       'error',
       {
