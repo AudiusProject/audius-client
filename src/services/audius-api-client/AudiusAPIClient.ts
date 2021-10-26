cfae516c25215ae15d262b44f934850d044a2e65
Saliou Diallo <saliou@audius.co>
2021-10-26 13:58:24 -0400

    Fix followee user id for optimistic social feed

--- src/services/audius-api-client/AudiusAPIClient.ts
+++ src/services/audius-api-client/AudiusAPIClient.ts
@@ -319,7 +319,7 @@ export type GetSocialFeedArgs = QueryParams & {
   filter: string
   with_users?: boolean
   tracks_only?: boolean
-  followee_user_ids?: ID[]
+  followee_user_id?: ID[]
   current_user_id?: ID
 }
 
