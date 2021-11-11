7d4225db3cf0b38ef8aa4c464456d626eb80c577
KJ Shanks <kyle.j.shanks@gmail.com>
2021-11-10 22:55:05 -0500

    Update collectibles routes

--- packages/bedtime/src/routes.js
+++ packages/bedtime/src/routes.js
@@ -3,5 +3,5 @@ const ROUTE_PREFIX = process.env.PREACT_APP_HOST_PREFIX
 export const ID_ROUTE = `${ROUTE_PREFIX}/:type`
 export const HASH_ID_ROUTE = `${ROUTE_PREFIX}/:type/:hashId`
 
-export const COLLECTIBLES_ROUTE = `${ROUTE_PREFIX}/collectibles/:handle`
-export const COLLECTIBLE_ID_ROUTE = `${ROUTE_PREFIX}/collectibles/:handle/:collectibleId`
+export const COLLECTIBLES_ROUTE = `${ROUTE_PREFIX}/:handle/collectibles`
+export const COLLECTIBLE_ID_ROUTE = `${ROUTE_PREFIX}/:handle/collectibles/:collectibleId`
