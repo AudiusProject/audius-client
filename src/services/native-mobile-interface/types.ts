653426c57401427f0deea158360f0293f7d08651
Saliou Diallo <saliou@audius.co>
2021-11-19 13:36:58 -0500

    Update hcapctha

--- src/services/native-mobile-interface/types.ts
+++ src/services/native-mobile-interface/types.ts
@@ -122,7 +122,10 @@ export enum MessageType {
   // Theme
   THEME_CHANGE = 'theme-change',
 
-  SYNC_CLIENT_STORE = 'sync-client-store'
+  SYNC_CLIENT_STORE = 'sync-client-store',
+
+  // hCaptcha
+  UPDATE_HCAPTCHA_SCORE = 'UPDATE_HCAPTCHA_SCORE'
 }
 
 export interface Message {
