2f8902efb5630919846de7018d63b6f12e24350a
Marcus Pasell <rickyrombo@gmail.com>
2021-11-20 03:51:04 +0000

    Add deactivate page UI for desktop and mobile web

---
+++ src/containers/deactivate-account-page/DeactivateAccountPage.tsx
@@ -0,0 +1,258 @@
+import React, { useCallback, useEffect, useState, useContext } from 'react'
+
+import { Button, ButtonType, Modal } from '@audius/stems'
+import cn from 'classnames'
+import { useDispatch, useSelector } from 'react-redux'
+
+import Status from 'common/models/Status'
+import ActionDrawer from 'components/action-drawer/ActionDrawer'
+import MobilePageContainer from 'components/general/MobilePageContainer'
+import Page from 'components/general/Page'
+import Header from 'components/general/header/desktop/Header'
+import { useMobileHeader } from 'components/general/header/mobile/hooks'
+import LoadingSpinnerFullPage from 'components/loading-spinner-full-page/LoadingSpinnerFullPage'
+import NavContext, {
+  LeftPreset,
+  RightPreset
+} from 'containers/nav/store/context'
+import { isMobile } from 'utils/clientUtil'
+import { BASE_URL, DEACTIVATE_PAGE } from 'utils/route'
+
+import styles from './DeactivateAccountPage.module.css'
+import { getDeactivateAccountStatus } from './store/selectors'
+import { deactivateAccount } from './store/slice'
+
+const messages = {
+  title: 'Deactivate',
+  description: 'Deactivate your account',
+  header: 'Are you sure you want to deactivate your account?',
+  listItems: [
+    "There's no going back.",
+    'This will remove all of your tracks, albums and playlists.',
+    'You will not be able to re-register with the same email or handle'
+  ],
+  confirmTitle: 'Deactivate Account',
+  confirm: 'Are you sure? This cannot be undone.',
+  buttonDeactivate: 'Deactivate',
+  buttonSafety: 'Take me back to safety',
+  buttonGoBack: 'Go Back'
+}
+
+type DeactivateAccountPageProps = {
+  children: React.ReactNode
+  isConfirmationVisible: boolean
+  isLoading: boolean
+  openConfirmation: () => void
+  onConfirm: () => void
+  closeConfirmation: () => void
+  onDrawerSelection: (rowNum: number) => void
+}
+
+type DeactivateAccountPageContentsProps = {
+  isMobile?: boolean
+  isLoading?: boolean
+  openConfirmation: () => void
+}
+
+type DeactivateAccountModalProps = {
+  isVisible: boolean
+  isLoading: boolean
+  onClose: () => void
+  onConfirm: () => void
+}
+
+export const DeactivateAcccountPageContents = ({
+  openConfirmation,
+  isMobile,
+  isLoading
+}: DeactivateAccountPageContentsProps) => {
+  return (
+    <div className={cn(styles.tile, { [styles.mobile]: isMobile })}>
+      <div className={styles.header}>{messages.header}</div>
+      <ul className={styles.list}>
+        {messages.listItems.map((message, i) => (
+          <li key={i}>{message}</li>
+        ))}
+      </ul>
+      {isLoading && isMobile && <LoadingSpinnerFullPage />}
+      <div className={styles.buttons}>
+        <Button
+          className={cn(styles.button, {
+            [styles.buttonDanger]: !(isLoading && isMobile)
+          })}
+          text={messages.buttonDeactivate}
+          type={
+            isLoading && isMobile ? ButtonType.DISABLED : ButtonType.PRIMARY_ALT
+          }
+          onClick={openConfirmation}
+        />
+        <Button
+          className={styles.button}
+          text={messages.buttonSafety}
+          type={
+            isLoading && isMobile ? ButtonType.DISABLED : ButtonType.PRIMARY_ALT
+          }
+        />
+      </div>
+    </div>
+  )
+}
+
+export const DeactivateAccountConfirmationModal = ({
+  isVisible,
+  onClose,
+  isLoading,
+  onConfirm
+}: DeactivateAccountModalProps) => {
+  return (
+    <Modal
+      bodyClassName={styles.confirmModal}
+      isOpen={isVisible}
+      onClose={onClose}
+      showDismissButton
+      showTitleHeader
+      title={messages.confirmTitle}
+    >
+      <div className={styles.container}>
+        {isLoading ? (
+          <LoadingSpinnerFullPage />
+        ) : (
+          <div className={styles.confirmText}>{messages.confirm}</div>
+        )}
+        <div className={styles.buttons}>
+          <Button
+            className={cn(styles.button, {
+              [styles.buttonDanger]: !isLoading
+            })}
+            isDisabled={isLoading}
+            onClick={onConfirm}
+            textClassName={styles.deleteButtonText}
+            text={messages.buttonDeactivate}
+            type={isLoading ? ButtonType.DISABLED : ButtonType.PRIMARY_ALT}
+          />
+          <Button
+            className={styles.button}
+            isDisabled={isLoading}
+            onClick={onClose}
+            text={messages.buttonGoBack}
+            type={isLoading ? ButtonType.DISABLED : ButtonType.PRIMARY_ALT}
+          />
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export const DeactivateAccountPageDesktop = ({
+  children,
+  isConfirmationVisible,
+  isLoading,
+  onConfirm,
+  closeConfirmation
+}: DeactivateAccountPageProps) => {
+  return (
+    <Page
+      title={messages.title}
+      description={messages.description}
+      header={<Header primary={messages.title} />}
+    >
+      {children}
+      <DeactivateAccountConfirmationModal
+        isVisible={isConfirmationVisible}
+        onClose={closeConfirmation}
+        onConfirm={onConfirm}
+        isLoading={isLoading}
+      />
+    </Page>
+  )
+}
+
+const useMobileNavContext = () => {
+  useMobileHeader({ title: messages.title })
+  const { setLeft, setRight } = useContext(NavContext)!
+  useEffect(() => {
+    setLeft(LeftPreset.CLOSE)
+    setRight(RightPreset.SEARCH)
+  }, [setLeft, setRight])
+}
+
+export const DeactivateAccountPageMobile = ({
+  children,
+  isConfirmationVisible,
+  onDrawerSelection,
+  closeConfirmation
+}: DeactivateAccountPageProps) => {
+  useMobileNavContext()
+  return (
+    <MobilePageContainer
+      title={messages.title}
+      description={messages.description}
+      canonicalUrl={`${BASE_URL}${DEACTIVATE_PAGE}`}
+      hasDefaultHeader
+    >
+      {children}
+      <ActionDrawer
+        isOpen={isConfirmationVisible}
+        onClose={closeConfirmation}
+        actions={[
+          { text: messages.buttonDeactivate, isDestructive: true },
+          { text: messages.buttonGoBack }
+        ]}
+        didSelectRow={onDrawerSelection}
+        title={messages.confirm}
+      />
+    </MobilePageContainer>
+  )
+}
+
+export const DeactivateAccountPage = () => {
+  const Page = isMobile()
+    ? DeactivateAccountPageMobile
+    : DeactivateAccountPageDesktop
+  const [isConfirmationVisible, setIsConfirmationVisible] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const deactivateAccountStatus = useSelector(getDeactivateAccountStatus)
+  const isDeactivating = deactivateAccountStatus === Status.LOADING
+
+  const openConfirmation = useCallback(() => {
+    setIsConfirmationVisible(true)
+  }, [setIsConfirmationVisible])
+  const closeConfirmation = useCallback(() => {
+    if (!isDeactivating) {
+      setIsConfirmationVisible(false)
+    }
+  }, [setIsConfirmationVisible, isDeactivating])
+  const onConfirm = useCallback(() => {
+    dispatch(deactivateAccount())
+  }, [dispatch])
+  const onDrawerSelection = useCallback(
+    (rowNumber: number) => {
+      if (rowNumber === 0) {
+        onConfirm()
+        closeConfirmation()
+      } else {
+        closeConfirmation()
+      }
+    },
+    [onConfirm, closeConfirmation]
+  )
+
+  return (
+    <Page
+      openConfirmation={openConfirmation}
+      isConfirmationVisible={isConfirmationVisible}
+      closeConfirmation={closeConfirmation}
+      onDrawerSelection={onDrawerSelection}
+      onConfirm={onConfirm}
+      isLoading={isDeactivating}
+    >
+      <DeactivateAcccountPageContents
+        isMobile={isMobile()}
+        isLoading={isDeactivating}
+        openConfirmation={openConfirmation}
+      />
+    </Page>
+  )
+}
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -7,6 +7,7 @@ import artistRecommendations from 'containers/artist-recommendations/store/slice
 import rewardsPage from 'containers/audio-rewards-page/store/slice'
 import changePassword from 'containers/change-password/store/slice'
 import collection from 'containers/collection-page/store/reducer'
+import deactivateAccount from 'containers/deactivate-account-page/store/slice'
 import deleted from 'containers/deleted-page/store/slice'
 import embedModal from 'containers/embed-modal/store/reducers'
 import exploreCollections from 'containers/explore-page/store/collections/slice'
@@ -117,6 +118,7 @@ const createRootReducer = routeHistory =>
         averageColor,
         changePassword,
         cookieBanner,
+        deactivateAccount,
         editPlaylistModal,
         editTrackModal,
         embedModal,
