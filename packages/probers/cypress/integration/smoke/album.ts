6a66643205158ebf47fd3a3af261d6c7bd7720df
Dylan Jeffers <dylan@audius.co>
2022-01-21 21:03:10 -0800

    Add cypress (#41)
    Adds cypress and implements album smoke test

---
+++ packages/probers/cypress/integration/smoke/album.ts
@@ -0,0 +1,9 @@
+import { route, name } from "../../fixtures/album.json";
+
+describe("Smoke test -- album page", () => {
+  it("Should load an album page when visited!", () => {
+    cy.visit(route);
+
+    cy.findByRole("heading", { name }).should("exist");
+  });
+});
