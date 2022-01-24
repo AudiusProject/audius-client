8842eda284189fca9f58aa43e84bfbda3815c7ed
Dylan Jeffers <dylan@audius.co>
2022-01-24 13:32:55 -0800

    Refactor guest smoke tests with cypress (#42)

--- packages/probers/cypress/integration/smoke/album.ts
+++ packages/probers/cypress/integration/smoke/album.ts
@@ -3,7 +3,6 @@ import { route, name } from "../../fixtures/album.json";
 describe("Smoke test -- album page", () => {
   it("Should load an album page when visited!", () => {
     cy.visit(route);
-
-    cy.findByRole("heading", { name }).should("exist");
+    cy.findByRole("heading", { name, level: 1 }).should("exist");
   });
 });
