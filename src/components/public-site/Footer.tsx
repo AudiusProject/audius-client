5cf77761c0497e458e68023648abc8c3f7c2636e
Nikki Kang <kangaroo233@gmail.com>
2022-02-07 08:57:30 -0800

    use date instead of moment

--- src/components/public-site/Footer.tsx
+++ src/components/public-site/Footer.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { IconInstagram, IconTwitterBird, IconDiscord } from '@audius/stems'
 import cn from 'classnames'
-import moment from 'moment'
 
 import horizontalLogo from 'assets/img/publicSite/Horizontal-Logo-Full-Color@2x.png'
 import {
@@ -144,7 +143,7 @@ const Footer = (props: FooterProps) => {
             ))}
           </div>
           <div className={styles.rightsContainer}>
-            <div>{messages.copyright(moment().year())}</div>
+            <div>{messages.copyright(new Date().getFullYear())}</div>
             <div>
               <span>{messages.madeWith}</span>
               <span className={styles.heart}>{messages.love}</span>
