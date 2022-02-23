5606c583e64dcbc05bb684d6b1e169ff39800142
Dylan Jeffers <dylan@audius.co>
2022-02-22 18:58:54 -0800

    Extract AppearanceSettingsRow

--- packages/mobile/src/components/core/Screen.tsx
+++ packages/mobile/src/components/core/Screen.tsx
@@ -1,24 +1,45 @@
 import { ReactElement, ReactNode, useEffect } from 'react'
 
 import { useNavigation } from '@react-navigation/native'
+import { Nullable } from 'audius-client/src/common/utils/typeUtils'
+import { StyleProp, View, ViewStyle } from 'react-native'
+
+import { makeStyles } from 'app/styles'
+
+const useStyles = makeStyles(({ palette }, { variant }) => ({
+  root: {
+    height: '100%',
+    backgroundColor:
+      variant === 'primary' ? palette.background : palette.backgroundSecondary
+  }
+}))
 
 type ScreenProps = {
   children: ReactNode
-  topbarLeft?: ReactElement
-  topbarRight?: ReactElement
+  topbarLeft?: Nullable<ReactElement>
+  topbarRight?: Nullable<ReactElement>
   title?: string
+  style?: StyleProp<ViewStyle>
+  variant?: 'primary' | 'secondary'
 }
 export const Screen = (props: ScreenProps) => {
-  const { children, topbarLeft, topbarRight, title } = props
+  const {
+    children,
+    topbarLeft,
+    topbarRight,
+    title,
+    variant = 'primary'
+  } = props
+  const styles = useStyles({ variant })
   const navigation = useNavigation()
 
   useEffect(() => {
     navigation.setOptions({
-      headerLeft: topbarLeft ? () => topbarLeft : undefined,
-      headerRight: topbarRight ? () => topbarRight : undefined,
+      headerLeft: topbarLeft === undefined ? undefined : () => topbarLeft,
+      headerRight: topbarRight === undefined ? undefined : () => topbarRight,
       title
     })
   }, [navigation, topbarLeft, topbarRight, title])
 
-  return <>{children}</>
+  return <View style={styles.root}>{children}</View>
 }
--- packages/mobile/src/screens/settings-screen/SettingsScreen.tsx
+++ packages/mobile/src/screens/settings-screen/SettingsScreen.tsx
@@ -4,23 +4,19 @@ import audiusLogoHorizontal from 'app/assets/images/Horizontal-Logo-Full-Color.p
 import Bell from 'app/assets/images/emojis/bell.png'
 import Headphone from 'app/assets/images/emojis/headphone.png'
 import SpeechBalloon from 'app/assets/images/emojis/speech-balloon.png'
-import Appearance from 'app/assets/images/emojis/waning-crescent-moon.png'
-import { Screen, SegmentedControl } from 'app/components/core'
+import { Screen } from 'app/components/core'
 import { makeStyles } from 'app/styles'
 
 import { AccountSettingsRow } from './AccountSettingsRow'
+import { AppearanceSettingsRow } from './AppearanceSettingsRow'
 import { Divider } from './Divider'
 import { SettingsRowLabel } from './SettingRowLabel'
 import { SettingsRow } from './SettingsRow'
-import { SettingsRowDescription } from './SettingsRowDescription'
 
 const messages = {
   title: 'Settings',
   listeningHistory: 'Listening History',
   notifications: 'Notifications',
-  appearance: 'Appearance',
-  appearanceDescription:
-    "Enable dark mode or choose 'Auto' to change with your system settings",
   about: 'About'
 }
 
@@ -36,7 +32,7 @@ const useStyles = makeStyles(() => ({
 export const SettingsScreen = () => {
   const styles = useStyles()
   return (
-    <Screen title={messages.title}>
+    <Screen title={messages.title} topbarRight={null} variant='secondary'>
       <Image source={audiusLogoHorizontal} style={styles.logo} />
       <AccountSettingsRow />
       <SettingsRow onPress={() => null}>
@@ -49,22 +45,7 @@ export const SettingsScreen = () => {
       <SettingsRow onPress={() => null}>
         <SettingsRowLabel label={messages.notifications} iconSource={Bell} />
       </SettingsRow>
-      <SettingsRow>
-        <SettingsRowLabel label={messages.appearance} iconSource={Appearance} />
-        <SettingsRowDescription>
-          {messages.appearanceDescription}
-        </SettingsRowDescription>
-        <SegmentedControl
-          fullWidth
-          options={[
-            { key: 'auto', text: 'Auto' },
-            { key: 'dark', text: 'Dark' },
-            { key: 'light', text: 'Light' }
-          ]}
-          defaultSelected='auto'
-          onSelectOption={() => {}}
-        />
-      </SettingsRow>
+      <AppearanceSettingsRow />
       <Divider />
       <SettingsRow onPress={() => null}>
         <SettingsRowLabel label={messages.about} iconSource={SpeechBalloon} />
