6f079fa89e6014f1644f79715d4298ae9e9c9ef8
Dylan Jeffers <dylan@audius.co>
2022-03-01 19:02:02 -0800

    replace custom equality with isEqual

--- packages/mobile/src/hooks/selectors.ts
+++ packages/mobile/src/hooks/selectors.ts
@@ -1,19 +1,13 @@
 import { getAccountUser } from 'audius-client/src/common/store/account/selectors'
 import { getProfileUser } from 'audius-client/src/common/store/pages/profile/selectors'
-import { isEmpty } from 'lodash'
+import { isEqual } from 'lodash'
 
 import { useSelectorWeb } from './useSelectorWeb'
 
 export const useProfile = () => {
-  return useSelectorWeb(
-    getProfileUser,
-    (a, b) =>
-      a?.user_id === b?.user_id &&
-      isEmpty(a?._cover_photo_sizes) === isEmpty(b?._cover_photo_sizes) &&
-      isEmpty(a?._profile_picture_sizes) === isEmpty(b?._profile_picture_sizes)
-  )
+  return useSelectorWeb(getProfileUser, isEqual)
 }
 
 export const useAccountUser = () => {
-  return useSelectorWeb(getAccountUser, (a, b) => a?.user_id === b?.user_id)
+  return useSelectorWeb(getAccountUser, isEqual)
 }
--- packages/mobile/src/screens/profile-screen/selectors.ts
+++ packages/mobile/src/screens/profile-screen/selectors.ts
@@ -1,58 +1,25 @@
-import { Collection } from 'audius-client/src/common/models/Collection'
 import { makeGetLineupMetadatas } from 'audius-client/src/common/store/lineup/selectors'
 import {
   getProfileFeedLineup,
   getProfileTracksLineup,
   makeGetProfile
 } from 'audius-client/src/common/store/pages/profile/selectors'
-import { Nullable } from 'audius-client/src/common/utils/typeUtils'
-import { isEmpty } from 'lodash'
+import { isEqual } from 'lodash'
 
 import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
 
 export const getProfile = makeGetProfile()
 
 export const useProfileTracksLineup = () => {
-  return useSelectorWeb(getProfileTracksLineup, (a, b) => {
-    return a.entries.length === b.entries.length
-  })
+  return useSelectorWeb(getProfileTracksLineup, isEqual)
 }
 
-const hasCoverArt = (collection: Collection) =>
-  !isEmpty(collection._cover_art_sizes)
+export const useProfileAlbums = () => useSelectorWeb(getProfile, isEqual)
 
-const areCollectionsEqual = (
-  a?: Nullable<Collection[]>,
-  b?: Nullable<Collection[]>
-) => {
-  return Boolean(
-    a &&
-      b &&
-      a.length === b.length &&
-      a.filter(hasCoverArt).length === b.filter(hasCoverArt).length
-  )
-}
-
-export const useProfileAlbums = () =>
-  useSelectorWeb(getProfile, (a, b) => {
-    return (
-      a.profile?.user_id === b.profile?.user_id &&
-      areCollectionsEqual(a?.albums, b?.albums)
-    )
-  })
-
-export const useProfilePlaylists = () =>
-  useSelectorWeb(getProfile, (a, b) => {
-    return (
-      a.profile?.user_id === b.profile?.user_id &&
-      areCollectionsEqual(a?.playlists, b?.playlists)
-    )
-  })
+export const useProfilePlaylists = () => useSelectorWeb(getProfile, isEqual)
 
 const getUserFeedMetadatas = makeGetLineupMetadatas(getProfileFeedLineup)
 
 export const useProfileFeedLineup = () => {
-  return useSelectorWeb(getUserFeedMetadatas, (a, b) => {
-    return a.entries.length === b.entries.length
-  })
+  return useSelectorWeb(getUserFeedMetadatas, isEqual)
 }
