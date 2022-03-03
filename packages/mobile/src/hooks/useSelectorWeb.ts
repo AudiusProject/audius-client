a998a834c75b1d9b222a2ed20767399df4ceb12d
Dylan Jeffers <dylan@audius.co>
2022-03-03 12:40:39 -0800

    [AUD-1642] Reduce overall app rerenders

--- packages/mobile/src/hooks/useSelectorWeb.ts
+++ packages/mobile/src/hooks/useSelectorWeb.ts
@@ -1,11 +1,12 @@
 import { CommonState } from 'audius-client/src/common/store'
+import { isEqual } from 'lodash'
 import { Selector, useSelector } from 'react-redux'
 
 // When mobile client is no longer dependent on the web client
 // calls to useSelectorWeb can be replaced with useSelector
 export const useSelectorWeb = <ReturnValue>(
   selector: Selector<CommonState, ReturnValue>,
-  equalityFn?: (left: ReturnValue, right: ReturnValue) => boolean
+  equalityFn: (left: ReturnValue, right: ReturnValue) => boolean = isEqual
 ) => {
   return useSelector(
     (state: { common: CommonState }) => selector(state.common),
