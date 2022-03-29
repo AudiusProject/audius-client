1a55106a9cf78168ba3984bec4fc35e18e2f9f87
Dylan Jeffers <dylan@audius.co>
2022-03-28 20:29:09 -0700

    [AUD-1791] Combine audio tier code for web and mobile

---
+++ packages/mobile/src/hooks/useSelectTierinfo.ts
@@ -0,0 +1,19 @@
+import { ID } from 'audius-client/src/common/models/Identifiers'
+import { makeGetTierAndVerifiedForUser } from 'audius-client/src/common/store/wallet/utils'
+
+import { useSelectorWeb } from './useSelectorWeb'
+
+const getTierAndVerifiedForUser = makeGetTierAndVerifiedForUser()
+
+/**
+ * Wraps our reselect tier selector in useMemo and useSelector
+ * to be safe for use in multiple components
+ *
+ * This was copied over from audius-client and useSelector was replaced
+ * with useSelectorWeb.
+ */
+export const useSelectTierInfo = (userId: ID) => {
+  return useSelectorWeb(state => {
+    return getTierAndVerifiedForUser(state, { userId })
+  })
+}
