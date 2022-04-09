9f68b8198cbaa6c03a1335746a90b8cbf0889c64
sliptype <sliptype@gmail.com>
2022-04-09 17:00:53 -0500

    Configure all deep link routes

--- packages/mobile/src/components/navigation-container/NavigationContainer.tsx
+++ packages/mobile/src/components/navigation-container/NavigationContainer.tsx
@@ -1,10 +1,14 @@
 import { ReactNode, useContext } from 'react'
 
 import {
+  getStateFromPath,
   LinkingOptions,
   NavigationContainer as RNNavigationContainer
 } from '@react-navigation/native'
+import { getAccountUser } from 'audius-client/src/common/store/account/selectors'
 
+import { usePushRouteWeb } from 'app/hooks/usePushRouteWeb'
+import { useSelectorWeb } from 'app/hooks/useSelectorWeb'
 import { RootScreenParamList } from 'app/screens/root-screen/RootScreen'
 
 import { ThemeContext } from '../theme/ThemeContext'
@@ -14,84 +18,97 @@ import { navigationThemes } from './navigationThemes'
 type Props = {
   children: ReactNode
 }
-
-const linking: LinkingOptions<RootScreenParamList> = {
-  prefixes: ['https://audius.co'],
-  // configuration for matching screens with paths
-  config: {
-    screens: {
-      App: {
-        initialRouteName: 'feed',
-        screens: {
-          feed: {
-            screens: {
-              FeedStack: 'feed',
-              Track: '*/*',
-              Profile: '*'
-            }
-          },
-          trending: {
-            screens: {
-              TrendingStack: {
-                screens: {
-                  ThisWeek: 'trending/thisWeek',
-                  ThisMonth: 'trending/thisMonth',
-                  ThisYear: 'trending/thisYear'
-                }
-              }
-            }
-          },
-          explore: {
-            screens: {
-              ExploreStack: {
-                screens: {
-                  forYou: 'explore/forYou',
-                  moods: 'explore/moods',
-                  playlists: 'explore/playlists',
-                  artists: 'explore/artists'
-                }
-              }
-            }
-          },
-          favorites: {
-            screens: {
-              FavoritesStack: {
-                screens: {
-                  tracks: 'favorites/tracks',
-                  albums: 'favorites/albums',
-                  playlists: 'favorites/playlists'
-                }
-              }
-            }
-          },
-          profile: {
-            screens: {
-              ProfileStack: {
-                screens: {
-                  Tracks: '*/tracks',
-                  Albums: '*/albums',
-                  Playlists: '*/playlists',
-                  Reposts: '*/reposts',
-                  Collectibles: '*/collectibles/*'
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
 /**
  * NavigationContainer contains the react-navigation context
  * and configures linking
  */
 const NavigationContainer = ({ children }: Props) => {
   const { theme, isSystemDarkMode } = useContext(ThemeContext)
+  const pushRouteWeb = usePushRouteWeb()
+  const account = useSelectorWeb(getAccountUser)
 
   const navigationTheme =
     theme === 'auto' ? (isSystemDarkMode ? 'dark' : 'default') : theme
+
+  const linking: LinkingOptions<RootScreenParamList> = {
+    prefixes: ['https://audius.co', 'https://staging.audius.co'],
+    // configuration for matching screens with paths
+    config: {
+      screens: {
+        App: {
+          screens: {
+            MainStack: {
+              initialRouteName: 'feed',
+              screens: {
+                feed: {
+                  initialRouteName: 'Feed',
+                  screens: {
+                    Feed: '/feed',
+                    Collection: '*/playlist/*',
+                    Track: '*/*',
+                    Profile: ':handle'
+                  }
+                },
+                trending: {
+                  initialRouteName: 'Trending',
+                  screens: {
+                    Trending: 'trending'
+                  }
+                },
+                explore: {
+                  initialRouteName: 'Explore',
+                  screens: {
+                    Explore: 'explore',
+                    TrendingPlaylists: 'explore/playlists',
+                    TrendingUnderground: 'explore/underground',
+                    LetThemDJ: 'explore/let-them-dj',
+                    TopAlbums: 'explore/top-albums',
+                    UnderTheRadar: 'explore/under-the-radar',
+                    BestNewReleases: 'explore/best-new-releases',
+                    Remixables: 'explore/remixables',
+                    MostLoved: 'explore/most-loved',
+                    FeelingLucky: 'explore/feeling-lucky',
+                    HeavyRotation: 'explore/heavy-rotation',
+                    ChillPlaylists: 'explore/chill',
+                    IntensePlaylists: 'explore/intense',
+                    IntimatePlaylists: 'explore/intimate',
+                    ProvokingPlaylists: 'explore/provoking',
+                    UpbeatPlaylists: 'explore/upbeat'
+                  }
+                },
+                favorites: {
+                  screens: {
+                    Favorites: 'favorites'
+                  }
+                },
+                profile: {
+                  screens: {
+                    UserProfile: 'profile'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    getStateFromPath: (path, options) => {
+      // Strip the trending query param because `/trending` will
+      // always go to ThisWeek
+      if (path.match(/^\/trending/)) {
+        path = '/trending'
+      }
+
+      pushRouteWeb(path, undefined, false)
+
+      // Check if the path is the current user and set path as `/profile`
+      if (path.replace('/', '') === account?.handle) {
+        path = '/profile'
+      }
+
+      return getStateFromPath(path, options)
+    }
+  }
 
   return (
     <RNNavigationContainer
