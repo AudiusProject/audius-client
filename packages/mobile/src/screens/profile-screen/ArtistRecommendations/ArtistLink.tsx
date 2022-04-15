bcad37da863cc5d5f741e5e42c624eabaa0ceb0a
sliptype <sliptype@gmail.com>
2022-04-15 10:28:06 -0500

    Make artist suggestions artist links pressable

--- packages/mobile/src/screens/profile-screen/ArtistRecommendations/ArtistLink.tsx
+++ packages/mobile/src/screens/profile-screen/ArtistRecommendations/ArtistLink.tsx
@@ -1,23 +1,38 @@
+import { useCallback } from 'react'
+
 import { User } from 'audius-client/src/common/models/User'
-import { Pressable } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 
 import { Text } from 'app/components/core'
 import UserBadges from 'app/components/user-badges'
+import { useNavigation } from 'app/hooks/useNavigation'
 
 type ArtistLinkProps = {
   artist: User
 }
 
 export const ArtistLink = (props: ArtistLinkProps) => {
+  const navigation = useNavigation()
+
   const { artist } = props
   const { name } = artist
 
+  const handleArtistPress = useCallback(() => {
+    navigation.push({
+      native: { screen: 'Profile', params: { handle: artist.handle } },
+      web: { route: `/${artist.handle}` }
+    })
+  }, [navigation, artist])
+
   return (
-    <Pressable style={{ flexDirection: 'row' }}>
+    <TouchableOpacity
+      style={{ flexDirection: 'row' }}
+      onPress={handleArtistPress}
+    >
       <Text color='secondary' variant='h3'>
         {name}
       </Text>
       <UserBadges user={artist} hideName badgeSize={8} />
-    </Pressable>
+    </TouchableOpacity>
   )
 }
