c0fb2fec7cc97540052b74bbed0b75a3b69191d6
Dylan Jeffers <dylan@audius.co>
2022-05-12 16:59:18 -0700

    [PAY-190] Redesign mobile web notifications

--- packages/web/src/components/notification/Notifications/RemixCosignNotification.tsx
+++ packages/web/src/components/notification/Notifications/RemixCosignNotification.tsx
@@ -8,14 +8,13 @@ import { RemixCosign, TrackEntity } from 'common/store/notifications/types'
 import { make, useRecord } from 'store/analytics/actions'
 import { openTwitterLink } from 'utils/tweet'
 
-import TrackContent from '../components/TrackContent'
-
 import { EntityLink } from './EntityLink'
 import { NotificationBody } from './NotificationBody'
 import { NotificationFooter } from './NotificationFooter'
 import { NotificationHeader } from './NotificationHeader'
 import { NotificationTile } from './NotificationTile'
 import { NotificationTitle } from './NotificationTitle'
+import { TrackContent } from './TrackContent'
 import { TwitterShareButton } from './TwitterShareButton'
 import { UserNameLink } from './UserNameLink'
 import { IconRemix } from './icons'
@@ -26,25 +25,6 @@ const messages = {
   cosign: 'Co-signed your Remix of',
   shareTwitterText: (track: TrackEntity, handle: string) =>
     `My remix of ${track.title} was Co-Signed by ${handle} on @AudiusProject #Audius`
-}
-
-const getTwitterShareInfo = async (notification: RemixCosign) => {
-  const { entities, parentTrackUserId, childTrackId } = notification
-  const parentTrack = entities.find(t => t.owner_id === parentTrackUserId)
-  const childtrack = entities.find(t => t.track_id === childTrackId)
-
-  if (!parentTrack || !childtrack) return { text: '', link: '' }
-
-  const link = getEntityLink(childtrack, true)
-
-  let twitterHandle = await getTwitterHandleByUserHandle(
-    notification.user.handle
-  )
-  if (!twitterHandle) twitterHandle = notification.user.name
-  else twitterHandle = `@${twitterHandle}`
-  const text = messages.shareTwitterText(parentTrack, twitterHandle)
-
-  return { link, text }
 }
 
 type RemixCosignNotificationProps = {
@@ -66,22 +46,32 @@ export const RemixCosignNotification = (
   const dispatch = useDispatch()
   const record = useRecord()
 
-  const entity = entities.find(
-    track => track.track_id === notification.childTrackId
+  const childTrack = entities.find(
+    track => track.track_id === childTrackId
   ) as TrackEntity
 
-  const handleShare = useCallback(async () => {
-    const { link, text } = await getTwitterShareInfo(notification)
-    openTwitterLink(link, text)
-    record(make(Name.NOTIFICATIONS_CLICK_REMIX_COSIGN_TWITTER_SHARE, { text }))
-  }, [notification, record])
+  const parentTrack = entities.find(
+    track => track.track_id === childTrackId
+  ) as TrackEntity
 
   const handleClick = useCallback(() => {
-    const childTrack = entities.find(track => track.track_id === childTrackId)
-    if (childTrack) {
-      dispatch(push(getEntityLink(childTrack)))
-    }
-  }, [entities, childTrackId, dispatch])
+    dispatch(push(getEntityLink(childTrack)))
+  }, [childTrack, dispatch])
+
+  const handleTwitterShare = useCallback(async () => {
+    let twitterHandle = await getTwitterHandleByUserHandle(user.handle)
+    if (!twitterHandle) twitterHandle = user.name
+    else twitterHandle = `@${twitterHandle}`
+
+    const shareText = messages.shareTwitterText(parentTrack, twitterHandle)
+
+    openTwitterLink(getEntityLink(childTrack, true), shareText)
+    record(
+      make(Name.NOTIFICATIONS_CLICK_REMIX_COSIGN_TWITTER_SHARE, {
+        text: shareText
+      })
+    )
+  }, [user, parentTrack, childTrack, record])
 
   return (
     <NotificationTile notification={notification} onClick={handleClick}>
@@ -90,12 +80,13 @@ export const RemixCosignNotification = (
       </NotificationHeader>
       <NotificationBody>
         <UserNameLink user={user} notification={notification} />{' '}
-        {messages.cosign} <EntityLink entity={entity} entityType={entityType} />
+        {messages.cosign}{' '}
+        <EntityLink entity={childTrack} entityType={entityType} />
       </NotificationBody>
       <div>
-        <TrackContent notification={notification} goToEntityPage={() => {}} />
+        <TrackContent track={childTrack} />
       </div>
-      <TwitterShareButton onClick={handleShare} />
+      <TwitterShareButton onClick={handleTwitterShare} />
       <NotificationFooter timeLabel={timeLabel} isRead={isRead} />
     </NotificationTile>
   )
