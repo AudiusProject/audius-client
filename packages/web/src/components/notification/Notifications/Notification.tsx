8ea81a8fe89fb7457bfba69f7607ba1a37da65cb
Dylan Jeffers <dylan@audius.co>
2022-05-12 14:01:15 -0700

    Improve social notif click events

--- packages/web/src/components/notification/Notifications/Notification.tsx
+++ packages/web/src/components/notification/Notifications/Notification.tsx
@@ -42,7 +42,7 @@ export const Notification = (props: NotificationProps) => {
   )
 
   const users = useSelector((state: CommonState) =>
-    getNotificationUsers(state, notificationProp, USER_LENGTH_LIMIT)
+    getNotificationUsers(state, notificationProp, USER_LENGTH_LIMIT + 3)
   )
 
   const entity = useSelector((state: CommonState) =>
