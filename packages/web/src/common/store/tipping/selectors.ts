923ff1551a35779ff6d161dcfaf8e02b23b2ff91
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-06-02 12:50:17 -0700

    Add better types

--- packages/web/src/common/store/tipping/selectors.ts
+++ packages/web/src/common/store/tipping/selectors.ts
@@ -44,7 +44,9 @@ export const getTipToDisplay = (state: CommonState) =>
 export const getShowTip = (state: CommonState) => state.tipping.showTip
 export const getMainUser = (state: CommonState) => state.tipping.mainUser
 
-const mergeMaps = <MapType extends Record<ID, Record<ID, any>>>({
+const mergeMaps = <
+  MapType extends Record<ID, Record<ID, Supporter | Supporting>>
+>({
   map,
   mapOverrides
 }: {
