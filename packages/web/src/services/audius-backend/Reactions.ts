1416ddaba2350bb42ce2f3a497d4bc7ed7051792
Saliou Diallo <saliou@audius.co>
2022-06-14 16:34:12 -0400

    Call libs to submit reaction to identity

---
+++ packages/web/src/services/audius-backend/Reactions.ts
@@ -0,0 +1,21 @@
+import { waitForLibsInit } from 'services/audius-backend/eagerLoadUtils'
+
+// @ts-ignore
+const libs = () => window.audiusLibs
+
+export const submitReaction = async ({
+  reactedTo,
+  reactionValue
+}: {
+  reactedTo: string
+  reactionValue: number
+}): Promise<{ success: boolean; error: any }> => {
+  try {
+    await waitForLibsInit()
+    return libs().Reactions.submitReaction({ reactedTo, reactionValue })
+  } catch (err) {
+    const errorMessage = (err as any).message
+    console.error(errorMessage)
+    return { success: false, error: errorMessage }
+  }
+}
