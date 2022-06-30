4fe8240c45fe0c03828904da6e8530518fa290af
Marcus Pasell <rickyrombo@gmail.com>
2022-06-30 17:35:24 +0000

    Add types file

---
+++ packages/web/src/components/audio-on-ramp-modal/types.ts
@@ -0,0 +1,9 @@
+export interface TokenListing {
+  chainId: number // 101,
+  address: string // 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+  symbol: string // 'USDC',
+  name: string // 'Wrapped USDC',
+  decimals: number // 6,
+  logoURI: string // 'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/BXXkv6z8ykpG1yuvUDPgh732wzVHB69RnB9YgSYh3itW/logo.png',
+  tags: string[] // [ 'stablecoin' ]
+}
