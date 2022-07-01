ace82a27f5fa6c88ccf7d177295c7481769d60ea
Nikki Kang <kangaroo233@gmail.com>
2022-07-01 10:40:25 -0400

    review

--- packages/stems/scripts/workers-site/index.js
+++ packages/stems/scripts/workers-site/index.js
@@ -17,46 +17,13 @@ addEventListener('fetch', event => {
   }
 })
 
-function checkIsBot(val) {
-  if (!val) {
-    return false
-  }
-  const botTest = new RegExp(
-    'altavista|baiduspider|bingbot|discordbot|duckduckbot|facebookexternalhit|gigabot|ia_archiver|linkbot|linkedinbot|msnbot|nextgensearchbot|reaper|slackbot|snap url preview service|telegrambot|twitterbot|whatsapp|whatsup|yahoo|yandex|yeti|yodaobot|zend|zoominfobot|embedly',
-    'i'
-  )
-  return botTest.test(val)
-}
-
 async function handleEvent(event) {
   const url = new URL(event.request.url)
-  const { pathname, search, hash } = url
+  const { pathname } = url
 
   const isUndefined = pathname === '/undefined'
   if (isUndefined) {
     return Response.redirect(url.origin, 302)
-  }
-
-  const userAgent = event.request.headers.get('User-Agent') || ''
-
-  const isBot = checkIsBot(userAgent)
-  const isEmbed = pathname.startsWith('/embed')
-  const is204 = pathname === '/204'
-
-  if (isBot || isEmbed || is204) {
-    const destinationURL = GA + pathname + search + hash
-    const newRequest = new Request(destinationURL, event.request)
-    newRequest.headers.set('host', GA)
-    newRequest.headers.set('x-access-token', GA_ACCESS_TOKEN)
-
-    return await fetch(newRequest)
-  }
-
-  const isSitemap = pathname.startsWith('/sitemaps')
-  if (isSitemap) {
-    const destinationURL = SITEMAP + pathname + search + hash
-    const newRequest = new Request(destinationURL, event.request)
-    return await fetch(newRequest)
   }
 
   const options = {}
