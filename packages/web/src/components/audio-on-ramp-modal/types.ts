9d5d1aab16e0e9495356c32dcfc944fd90f5982a
Marcus Pasell <rickyrombo@gmail.com>
2022-07-01 06:35:44 +0000

    Add new type for swap success

--- packages/web/src/components/audio-on-ramp-modal/types.ts
+++ packages/web/src/components/audio-on-ramp-modal/types.ts
@@ -1,3 +1,5 @@
+import { SwapResult } from '@jup-ag/core'
+
 export interface TokenListing {
   chainId: number // 101,
   address: string // 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
@@ -6,4 +8,8 @@ export interface TokenListing {
   decimals: number // 6,
   logoURI: string // 'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/BXXkv6z8ykpG1yuvUDPgh732wzVHB69RnB9YgSYh3itW/logo.png',
   tags: string[] // [ 'stablecoin' ]
+}
+
+export type SwapResultSuccess = SwapResult & {
+  outputAmount: number | undefined
 }
