1d16f884391c3b79cd6adfdcdfa70e6a6570d056
Marcus Pasell <rickyrombo@gmail.com>
2022-07-01 06:38:23 +0000

    add success page with confetti

---
+++ packages/web/src/components/audio-on-ramp-modal/SuccessPage.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react'
+
+import { useDispatch } from 'react-redux'
+
+import DynamicImage from 'components/dynamic-image/DynamicImage'
+import ConnectedMusicConfetti from 'components/music-confetti/ConnectedMusicConfetti'
+import { show as showConfetti } from 'components/music-confetti/store/slice'
+
+import styles from './SuccessPage.module.css'
+import { SwapResultSuccess, TokenListing } from './types'
+
+export const SuccessPage = ({
+  swapResult,
+  inputToken,
+  outputToken
+}: {
+  swapResult?: SwapResultSuccess
+  inputToken: TokenListing
+  outputToken: TokenListing
+}) => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(showConfetti())
+  }, [dispatch])
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.heading}>Congratulations!</h2>
+      <p>You successfully bought </p>
+      <div className={styles.token}>
+        <DynamicImage
+          className={styles.symbolLogo}
+          wrapperClassName={styles.symbolLogoContainer}
+          image={outputToken.logoURI}
+          isUrl={true}
+        />
+        <span>
+          {(swapResult?.outputAmount ?? 0) / 10 ** outputToken.decimals} $
+          {outputToken.symbol}
+        </span>
+      </div>
+      <ConnectedMusicConfetti />
+    </div>
+  )
+}
