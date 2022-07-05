cd18a6f61f0da1c6310407fd424b465d0bb8f848
Saliou Diallo <saliou@audius.co>
2022-07-05 06:38:28 -0400

    Introduce media listener hook and use it for the button component

---
+++ packages/stems/src/hooks/useMediaQueryListener.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState, useCallback } from 'react'
+
+export const useMediaQueryListener = (mediaQuery: string) => {
+  // Stores whether there is a match
+  const [isMatch, setIsMatch] = useState(false)
+  console.log('here in med que lis', isMatch)
+
+  // Updates whether there is a match or not
+  // when the media query status changes
+  const listener = useCallback(
+    matcher => setIsMatch(matcher.matches),
+    [setIsMatch]
+  )
+
+  // If mediaQuery is set, set up a media query listener
+  useEffect(() => {
+    if (mediaQuery) {
+      const matcher = window.matchMedia(mediaQuery)
+      listener(matcher)
+      matcher.addListener(listener)
+      return () => matcher.removeListener(listener)
+    }
+    return () => {}
+  }, [mediaQuery, listener])
+
+  return { isMatch }
+}
