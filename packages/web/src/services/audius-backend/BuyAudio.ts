d18fcf4028adbeea9c461b37c19d264b438ec3ca
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-07-30 01:26:08 +0000

    Add retries to polling for token account creation

--- packages/web/src/services/audius-backend/BuyAudio.ts
+++ packages/web/src/services/audius-backend/BuyAudio.ts
@@ -5,6 +5,9 @@ import { waitForLibsInit } from 'services/audius-backend/eagerLoadUtils'
 // @ts-ignore
 const libs = () => window.audiusLibs
 
+const TOKEN_ACCOUNT_POLL_MS = 5000
+const MAX_TOKEN_ACCOUNT_POLL_COUNT = 20
+
 export const getRootSolanaAccount = async () => {
   await waitForLibsInit()
   return libs().solanaWeb3Manager.solanaWeb3.Keypair.fromSeed(
@@ -30,10 +33,24 @@ export const createTransferToUserBankTransaction = async ({
     await libs().solanaWeb3Manager.findAssociatedTokenAddress(
       fromAccount.publicKey.toString()
     )
-  const tokenAccountInfo: AccountInfo =
+  let tokenAccountInfo: AccountInfo =
     await libs().solanaWeb3Manager.getAssociatedTokenAccountInfo(
       associatedTokenAccount.toString()
     )
+  let retries = 0
+  while (!tokenAccountInfo && retries < MAX_TOKEN_ACCOUNT_POLL_COUNT) {
+    console.debug(
+      `$AUDIO account not found. Retrying... ${retries}/${MAX_TOKEN_ACCOUNT_POLL_COUNT}`
+    )
+    retries++
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, TOKEN_ACCOUNT_POLL_MS)
+    })
+    tokenAccountInfo =
+      await libs().solanaWeb3Manager.getAssociatedTokenAccountInfo(
+        associatedTokenAccount.toString()
+      )
+  }
   if (!tokenAccountInfo) {
     throw new Error('No $AUDIO account in root wallet')
   }
