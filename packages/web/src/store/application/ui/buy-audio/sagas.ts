96de7aece6e34a75a30d9145090d4f089419e04b
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-08-02 01:34:30 +0000

    Move some stuff around and rename

--- packages/web/src/store/application/ui/buy-audio/sagas.ts
+++ packages/web/src/store/application/ui/buy-audio/sagas.ts
@@ -246,6 +246,240 @@ function* executeSwap({
 }
 
 /**
+ * Checks if the associated accounts necessary for a quoted `route` exist on `rootAccount`,
+ * and for those that don't, estimates the needed lamports to pay for rent exemption as they are created.
+ * Uses the redux store to cache the result.
+ * @returns the total amount of lamports necessary for all ATA creation rent fees in a swap and transfer
+ */
+function* getAssociatedAccountCreationFees({
+  route,
+  rootAccount,
+  feesCache
+}: {
+  route: RouteInfo
+  rootAccount: PublicKey
+  feesCache: ReturnType<typeof getFeesCache>
+}) {
+  const mintKeysSet = new Set<PublicKey>()
+  for (const marketInfo of route.marketInfos) {
+    mintKeysSet.add(marketInfo.inputMint)
+    mintKeysSet.add(marketInfo.outputMint)
+  }
+  const minRentForATA = yield* call(getAssociatedTokenRentExemptionMinimum)
+  let accountCreationFees = 0
+  for (const mintKey of mintKeysSet.values()) {
+    const exists = feesCache?.associatedTokenAccountCache[mintKey.toString()]
+    if (exists === false) {
+      accountCreationFees += minRentForATA
+    } else if (exists === undefined) {
+      const accountInfo = yield* call(getAssociatedTokenAccountInfo, {
+        rootAccount,
+        mintKey
+      })
+      yield* put(
+        cacheAssociatedTokenAccount({
+          account: mintKey.toString(),
+          exists: accountInfo !== null
+        })
+      )
+      if (accountInfo === null) {
+        accountCreationFees += minRentForATA
+      }
+    }
+  }
+  return accountCreationFees
+}
+
+/**
+ * Creates the transactions necessary for a swap and transfer to calculate transaction fees
+ * @returns the total amount of lamports necessary for all transaction fees in a swap and transfer
+ */
+function* getTransactionFees({
+  route,
+  rootAccount,
+  feesCache
+}: {
+  route: RouteInfo
+  rootAccount: PublicKey
+  feesCache: ReturnType<typeof getFeesCache>
+}) {
+  let transactionFees = feesCache?.transactionFees ?? 0
+  if (!transactionFees) {
+    const jup = yield* call(initJupiterIfNecessary)
+    const {
+      transactions: { setupTransaction, swapTransaction, cleanupTransaction }
+    } = yield* call([jup, jup.exchange], {
+      routeInfo: route,
+      userPublicKey: rootAccount
+    })
+    const userBank = yield* call(getUserBank)
+    const transferTransaction = yield* call(
+      createTransferToUserBankTransaction,
+      {
+        userBank,
+        fromAccount: rootAccount,
+        // eslint-disable-next-line new-cap
+        amount: new u64(JSBI.toNumber(route.outAmount))
+      }
+    )
+    const connection = yield* call(getSolanaConnection)
+    const latestBlockhashResult = yield* call(
+      [connection, connection.getLatestBlockhash],
+      'finalized'
+    )
+    const names = ['Setup', 'Swap', 'Cleanup', 'Transfer']
+    let i = 0
+    for (const transaction of [
+      setupTransaction,
+      swapTransaction,
+      cleanupTransaction,
+      transferTransaction
+    ]) {
+      if (transaction) {
+        if (!transaction.recentBlockhash) {
+          transaction.recentBlockhash = latestBlockhashResult.blockhash
+        }
+        if (!transaction.feePayer) {
+          transaction.feePayer = rootAccount
+        }
+        const message = transaction.compileMessage()
+        const fees = yield* call(
+          [connection, connection.getFeeForMessage],
+          message
+        )
+        console.debug(
+          `Fee for "${names[i]}" transaction: ${fees.value} Lamports`
+        )
+        transactionFees += fees.value
+      }
+      i++
+    }
+    yield* put(cacheTransactionFees({ transactionFees }))
+  }
+  return transactionFees
+}
+
+/**
+ * Calculates all the fees required for executing a swap and transfer by doing a "dry-run"
+ * @returns the transaction fees and ATA creation fees (in lamports)
+ */
+function* getSwapFees({ route }: { route: RouteInfo }) {
+  const feesCache = yield* select(getFeesCache)
+  const rootAccount = yield* call(getRootSolanaAccount)
+  const associatedAccountCreationFees = yield* call(
+    getAssociatedAccountCreationFees,
+    { rootAccount: rootAccount.publicKey, route, feesCache }
+  )
+
+  const transactionFees = yield* call(getTransactionFees, {
+    rootAccount: rootAccount.publicKey,
+    route,
+    feesCache
+  })
+  console.debug(
+    `Estimated transaction fees: ${
+      transactionFees / LAMPORTS_PER_SOL
+    } SOL. Estimated associated account creation fees: ${
+      associatedAccountCreationFees / LAMPORTS_PER_SOL
+    } SOL. Total estimated fees: ${
+      (associatedAccountCreationFees + transactionFees) / LAMPORTS_PER_SOL
+    }`
+  )
+  return { transactionFees, associatedAccountCreationFees }
+}
+
+function* getAudioPurchaseInfo({
+  payload: { audioAmount }
+}: ReturnType<typeof calculateAudioPurchaseInfo>) {
+  try {
+    // Ensure userbank is created
+    yield* fork(function* () {
+      yield* call(createUserBankIfNeeded)
+    })
+
+    // Setup
+    const connection = yield* call(getSolanaConnection)
+    const rootAccount = yield* call(getRootSolanaAccount)
+
+    const slippage = SLIPPAGE
+
+    // Get AUDIO => SOL quote
+    const reverseQuote = yield* call(getQuote, {
+      inputTokenSymbol: 'AUDIO',
+      outputTokenSymbol: 'SOL',
+      inputAmount: audioAmount,
+      slippage
+    })
+    const slippageFactor = 100.0 / (100.0 - slippage)
+
+    // Adjust quote for potential slippage
+    const inSol = Math.ceil(reverseQuote.outputAmount.amount * slippageFactor)
+
+    // Get SOL => AUDIO quote to calculate fees
+    const quote = yield* call(getQuote, {
+      inputTokenSymbol: 'SOL',
+      outputTokenSymbol: 'AUDIO',
+      inputAmount: inSol,
+      slippage
+    })
+    const { associatedAccountCreationFees, transactionFees } = yield* call(
+      getSwapFees,
+      { route: quote.route }
+    )
+
+    // Get existing solana balance
+    const existingBalance = yield* call(
+      [connection, connection.getBalance],
+      rootAccount.publicKey,
+      'finalized'
+    )
+
+    const estimatedLamports =
+      inSol + associatedAccountCreationFees + transactionFees - existingBalance
+
+    // Get SOL => USDC quote to estimate $USD cost
+    const quoteUSD = yield* call(getQuote, {
+      inputTokenSymbol: 'SOL',
+      outputTokenSymbol: 'USDC',
+      inputAmount: estimatedLamports / LAMPORTS_PER_SOL,
+      slippage: 0
+    })
+
+    console.debug(
+      `Quoted: ${reverseQuote.outputAmount.uiAmountString} SOL
+Adjustment For Slippage (${slippage}%): ${
+        (inSol - reverseQuote.outputAmount.amount) / LAMPORTS_PER_SOL
+      } SOL
+Fees: ${
+        (associatedAccountCreationFees + transactionFees) / LAMPORTS_PER_SOL
+      } SOL
+Existing Balance: ${existingBalance / LAMPORTS_PER_SOL} SOL
+Total: ${estimatedLamports / LAMPORTS_PER_SOL} SOL ($${
+        quoteUSD.outputAmount.uiAmountString
+      } USDC)`
+    )
+
+    yield* put(
+      calculateAudioPurchaseInfoSucceeded({
+        estimatedSOL: convertJSBIToAmountObject(
+          JSBI.BigInt(estimatedLamports),
+          TOKEN_LISTING_MAP.SOL.decimals
+        ),
+        estimatedUSD: quoteUSD.outputAmount,
+        desiredAudioAmount: convertJSBIToAmountObject(
+          JSBI.BigInt(
+            Math.ceil(audioAmount * 10 ** TOKEN_LISTING_MAP.AUDIO.decimals)
+          ),
+          TOKEN_LISTING_MAP.AUDIO.decimals
+        )
+      })
+    )
+  } catch (e) {
+    console.error('Failed to get AUDIO purchase info:', e)
+  }
+}
+
+/**
  * Exchanges all but the minimum balance required for a swap from a wallet once a balance change is seen
  */
 function* startBuyAudioFlow({
@@ -400,242 +634,8 @@ function* startBuyAudioFlow({
   }
 }
 
-/**
- * Checks if the associated accounts necessary for a quoted `route` exist on `rootAccount`,
- * and for those that don't, estimates the needed lamports to pay for rent exemption as they are created.
- * Uses the redux store to cache the result.
- * @returns the total amount of lamports necessary for all ATA creation rent fees in a swap and transfer
- */
-function* getAssociatedAccountCreationFees({
-  route,
-  rootAccount,
-  feesCache
-}: {
-  route: RouteInfo
-  rootAccount: PublicKey
-  feesCache: ReturnType<typeof getFeesCache>
-}) {
-  const mintKeysSet = new Set<PublicKey>()
-  for (const marketInfo of route.marketInfos) {
-    mintKeysSet.add(marketInfo.inputMint)
-    mintKeysSet.add(marketInfo.outputMint)
-  }
-  const minRentForATA = yield* call(getAssociatedTokenRentExemptionMinimum)
-  let accountCreationFees = 0
-  for (const mintKey of mintKeysSet.values()) {
-    const exists = feesCache?.associatedTokenAccountCache[mintKey.toString()]
-    if (exists === false) {
-      accountCreationFees += minRentForATA
-    } else if (exists === undefined) {
-      const accountInfo = yield* call(getAssociatedTokenAccountInfo, {
-        rootAccount,
-        mintKey
-      })
-      yield* put(
-        cacheAssociatedTokenAccount({
-          account: mintKey.toString(),
-          exists: accountInfo !== null
-        })
-      )
-      if (accountInfo === null) {
-        accountCreationFees += minRentForATA
-      }
-    }
-  }
-  return accountCreationFees
-}
-
-/**
- * Creates the transactions necessary for a swap and transfer to calculate transaction fees
- * @returns the total amount of lamports necessary for all transaction fees in a swap and transfer
- */
-function* getTransactionFees({
-  route,
-  rootAccount,
-  feesCache
-}: {
-  route: RouteInfo
-  rootAccount: PublicKey
-  feesCache: ReturnType<typeof getFeesCache>
-}) {
-  let transactionFees = feesCache?.transactionFees ?? 0
-  if (!transactionFees) {
-    const jup = yield* call(initJupiterIfNecessary)
-    const {
-      transactions: { setupTransaction, swapTransaction, cleanupTransaction }
-    } = yield* call([jup, jup.exchange], {
-      routeInfo: route,
-      userPublicKey: rootAccount
-    })
-    const userBank = yield* call(getUserBank)
-    const transferTransaction = yield* call(
-      createTransferToUserBankTransaction,
-      {
-        userBank,
-        fromAccount: rootAccount,
-        // eslint-disable-next-line new-cap
-        amount: new u64(JSBI.toNumber(route.outAmount))
-      }
-    )
-    const connection = yield* call(getSolanaConnection)
-    const latestBlockhashResult = yield* call(
-      [connection, connection.getLatestBlockhash],
-      'finalized'
-    )
-    const names = ['Setup', 'Swap', 'Cleanup', 'Transfer']
-    let i = 0
-    for (const transaction of [
-      setupTransaction,
-      swapTransaction,
-      cleanupTransaction,
-      transferTransaction
-    ]) {
-      if (transaction) {
-        if (!transaction.recentBlockhash) {
-          transaction.recentBlockhash = latestBlockhashResult.blockhash
-        }
-        if (!transaction.feePayer) {
-          transaction.feePayer = rootAccount
-        }
-        const message = transaction.compileMessage()
-        const fees = yield* call(
-          [connection, connection.getFeeForMessage],
-          message
-        )
-        console.debug(
-          `Fee for "${names[i]}" transaction: ${fees.value} Lamports`
-        )
-        transactionFees += fees.value
-      }
-      i++
-    }
-    yield* put(cacheTransactionFees({ transactionFees }))
-  }
-  return transactionFees
-}
-
-/**
- * Calculates all the fees required for executing a swap and transfer by doing a "dry-run"
- * @returns the transaction fees and ATA creation fees (in lamports)
- */
-function* getSwapFees({ route }: { route: RouteInfo }) {
-  const feesCache = yield* select(getFeesCache)
-  const rootAccount = yield* call(getRootSolanaAccount)
-  const associatedAccountCreationFees = yield* call(
-    getAssociatedAccountCreationFees,
-    { rootAccount: rootAccount.publicKey, route, feesCache }
-  )
-
-  const transactionFees = yield* call(getTransactionFees, {
-    rootAccount: rootAccount.publicKey,
-    route,
-    feesCache
-  })
-  console.debug(
-    `Estimated transaction fees: ${
-      transactionFees / LAMPORTS_PER_SOL
-    } SOL. Estimated associated account creation fees: ${
-      associatedAccountCreationFees / LAMPORTS_PER_SOL
-    } SOL. Total estimated fees: ${
-      (associatedAccountCreationFees + transactionFees) / LAMPORTS_PER_SOL
-    }`
-  )
-  return { transactionFees, associatedAccountCreationFees }
-}
-
-function* doCalculateAudioPurchaseInfo({
-  payload: { audioAmount }
-}: ReturnType<typeof calculateAudioPurchaseInfo>) {
-  try {
-    // Ensure userbank is created
-    yield* fork(function* () {
-      yield* call(createUserBankIfNeeded)
-    })
-
-    // Setup
-    const connection = yield* call(getSolanaConnection)
-    const rootAccount = yield* call(getRootSolanaAccount)
-
-    const slippage = SLIPPAGE
-
-    // Get AUDIO => SOL quote
-    const reverseQuote = yield* call(getQuote, {
-      inputTokenSymbol: 'AUDIO',
-      outputTokenSymbol: 'SOL',
-      inputAmount: audioAmount,
-      slippage
-    })
-    const slippageFactor = 100.0 / (100.0 - slippage)
-
-    // Adjust quote for potential slippage
-    const inSol = Math.ceil(reverseQuote.outputAmount.amount * slippageFactor)
-
-    // Get SOL => AUDIO quote to calculate fees
-    const quote = yield* call(getQuote, {
-      inputTokenSymbol: 'SOL',
-      outputTokenSymbol: 'AUDIO',
-      inputAmount: inSol,
-      slippage
-    })
-    const { associatedAccountCreationFees, transactionFees } = yield* call(
-      getSwapFees,
-      { route: quote.route }
-    )
-
-    // Get existing solana balance
-    const existingBalance = yield* call(
-      [connection, connection.getBalance],
-      rootAccount.publicKey,
-      'finalized'
-    )
-
-    const estimatedLamports =
-      inSol + associatedAccountCreationFees + transactionFees - existingBalance
-
-    // Get SOL => USDC quote to estimate $USD cost
-    const quoteUSD = yield* call(getQuote, {
-      inputTokenSymbol: 'SOL',
-      outputTokenSymbol: 'USDC',
-      inputAmount: estimatedLamports / LAMPORTS_PER_SOL,
-      slippage: 0
-    })
-
-    console.debug(
-      `Quoted: ${reverseQuote.outputAmount.uiAmountString} SOL
-Adjustment For Slippage (${slippage}%): ${
-        (inSol - reverseQuote.outputAmount.amount) / LAMPORTS_PER_SOL
-      } SOL
-Fees: ${
-        (associatedAccountCreationFees + transactionFees) / LAMPORTS_PER_SOL
-      } SOL
-Existing Balance: ${existingBalance / LAMPORTS_PER_SOL} SOL
-Total: ${estimatedLamports / LAMPORTS_PER_SOL} SOL ($${
-        quoteUSD.outputAmount.uiAmountString
-      } USDC)`
-    )
-
-    yield* put(
-      calculateAudioPurchaseInfoSucceeded({
-        estimatedSOL: convertJSBIToAmountObject(
-          JSBI.BigInt(estimatedLamports),
-          TOKEN_LISTING_MAP.SOL.decimals
-        ),
-        estimatedUSD: quoteUSD.outputAmount,
-        desiredAudioAmount: convertJSBIToAmountObject(
-          JSBI.BigInt(
-            Math.ceil(audioAmount * 10 ** TOKEN_LISTING_MAP.AUDIO.decimals)
-          ),
-          TOKEN_LISTING_MAP.AUDIO.decimals
-        )
-      })
-    )
-  } catch (e) {
-    console.error(e)
-  }
-}
-
 function* watchCalculateAudioPurchaseInfo() {
-  yield takeLatest(calculateAudioPurchaseInfo, doCalculateAudioPurchaseInfo)
+  yield takeLatest(calculateAudioPurchaseInfo, getAudioPurchaseInfo)
 }
 
 function* watchOnRampStarted() {
