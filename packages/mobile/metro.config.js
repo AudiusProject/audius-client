3747da1b3c0f9abbc8da14bff9533367d5c106ba
Dylan Jeffers <dylan@audius.co>
2022-08-03 15:37:49 -0700

    Final metro config

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -49,12 +49,7 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg', 'cjs'],
-      // disableHierarchicalLookup: true,
-      // blockList: /node_modules\/react$/,
-      nodeModulesPaths: [
-        path.resolve(__dirname, 'node_modules')
-        // path.resolve(clientPath, 'node_modules')
-      ],
+      nodeModulesPaths: [path.resolve(__dirname, 'node_modules')],
       extraNodeModules: {
         ...require('node-libs-react-native'),
         // Alias for 'src' to allow for absolute paths
@@ -65,19 +60,9 @@ module.exports = (async () => {
 
         // Various polyfills to enable @audius/sdk to run in react-native
         child_process: emptyPolyfill,
-        // constants: resolveModule('constants-browserify'),
-        // crypto: resolveModule('expo-crypto'),
         fs: resolveModule('react-native-fs'),
-        // http: emptyPolyfill,
-        // https: emptyPolyfill,
         net: emptyPolyfill,
-        // 'node-localstorage': emptyPolyfill,
-        // os: resolveModule('os-browserify'),
-        // path: resolveModule('path-browserify'),
-        // 'react-native': path.resolve(__dirname, 'node_modules/react-native'),
-        // stream: emptyPolyfill,
         tls: resolveModule('tls-browserify')
-        // zlib: resolveModule('browserify-zlib')
       }
     },
     maxWorkers: 2
