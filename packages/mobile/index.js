58dbe02f9157a0a32f04733a4cbd7a8e9bd1371e
Dylan Jeffers <dylan@audius.co>
2022-08-03 15:54:22 -0700

    clean up deps

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -4,6 +4,7 @@ import { AppRegistry, LogBox } from 'react-native'
 import { name as appName } from './app.json'
 
 require('node-libs-react-native/globals')
+
 const App = require('./src/App').default
 
 // Ignore LogBox logs for preferred log messages in external
