fef5b7881a9e910f3577d2a7fef07319915b2e38
dharit-tan <dharit.tan@gmail.com>
2022-08-11 13:36:30 -0400

    Do not expose notion doc

--- packages/web/src/common/utils/aaoErrorCodes.ts
+++ packages/web/src/common/utils/aaoErrorCodes.ts
@@ -12,14 +12,10 @@ const aaoErrorEmojis = [
 ]
 
 /**
- * Currently we have 7 error codes, please see this link:
- * https://www.notion.so/audiusproject/77229a671b564d97a7426ed98fe3c928?v=1b434faf2f7c4de19ea20f2d6c73ea9b
- *
- * Error code mappings:
  * Negative numbers are unexpected but valid, return last emoji.
  * Positive numbers are expected, return emoji at index of error code.
- * Positive numbers larger than number of error codes are technically invalid,
- * but ignore them and return last emoji to prevent errors.
+ * Positive numbers greater than total number of error codes are technically
+ * invalid, but ignore them and return last emoji to avoid errors.
  **/
 export const getAAOErrorEmojis = (errorCode: number): string => {
   return aaoErrorEmojis[errorCode] ?? aaoErrorEmojis[aaoErrorEmojis.length - 1]
