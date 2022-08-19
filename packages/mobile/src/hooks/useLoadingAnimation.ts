5d3ff33d40bc443dfb9aebbd741ce62c46db2571
dharit-tan <dharit.tan@gmail.com>
2022-08-19 17:20:53 -0400

    Remove LayoutAnimation android flag

--- packages/mobile/src/hooks/useLoadingAnimation.ts
+++ packages/mobile/src/hooks/useLoadingAnimation.ts
@@ -1,16 +1,10 @@
 import { useRef, useLayoutEffect } from 'react'
 
-import { LayoutAnimation, Platform, UIManager } from 'react-native'
+import { LayoutAnimation } from 'react-native'
 
 const useLoadingAnimation = (isDepLoaded: () => boolean, dependency: any) => {
   // Prevents multiple re-renders if the dependency changes.
   const isLoaded = useRef(false)
-
-  if (Platform.OS === 'android') {
-    if (UIManager.setLayoutAnimationEnabledExperimental) {
-      UIManager.setLayoutAnimationEnabledExperimental(true)
-    }
-  }
 
   useLayoutEffect(() => {
     if (isDepLoaded() && !isLoaded.current) {
