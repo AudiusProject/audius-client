d0b1e6fde3cadc22f5ac9af7c6e4ebd96c8c3f55
Dylan Jeffers <dylan@audius.co>
2022-08-19 12:01:58 -0700

    [C-826] Add env context, fix native eagerDiscovery

--- packages/mobile/src/services/audius-api-client/apiClient.ts
+++ packages/mobile/src/services/audius-api-client/apiClient.ts
@@ -1,11 +1,15 @@
 import { AudiusAPIClient } from 'audius-client/src/common/services/audius-api-client'
 
 import { audiusBackendInstance, audiusLibs } from '../audius-backend-instance'
+import { env } from '../env'
+import { localStorage } from '../local-storage'
 import { remoteConfigInstance } from '../remote-config'
 
 export const apiClient = new AudiusAPIClient({
   audiusBackendInstance,
   remoteConfigInstance,
   // @ts-expect-error -- currently using two different versions of @audius/sdk to unblock mobile work
-  audiusLibs
+  audiusLibs,
+  localStorage,
+  env
 })
