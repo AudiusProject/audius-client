e9d3fc0703a9c3e4fcfdea5daab132e367e38434
sliptype <sliptype@gmail.com>
2022-08-25 15:10:10 -0500

    Search results screen (#1782)
    * search results screen
    
    * remove comments
    
    Co-authored-by: Nikki Kang <kangaroo233@gmail.com>

--- packages/mobile/src/store/sagas.ts
+++ packages/mobile/src/store/sagas.ts
@@ -9,6 +9,7 @@ import usersSagas from 'common/store/cache/users/sagas'
 import confirmerSagas from 'common/store/confirmer/sagas'
 import signOnSagas from 'common/store/pages/signon/sagas'
 import trackPageSagas from 'common/store/pages/track/sagas'
+import searchBarSagas from 'common/store/search-bar/sagas'
 import signOutSagas from 'common/store/sign-out/sagas'
 import { all, fork } from 'typed-redux-saga'
 
@@ -23,6 +24,7 @@ export default function* rootSaga() {
     ...analyticsSagas(),
     ...accountSagas(),
     ...confirmerSagas(),
+    ...searchBarSagas(),
 
     // Cache
     ...coreCacheSagas(),
