cdedc5c3e05ffe0af2cba8fdcffa057f19bc282f
Dylan Jeffers <dylan@audius.co>
2022-08-29 09:47:04 -0700

    [C-872] Play audio in native (#1807)

--- packages/web/src/store/sagas.ts
+++ packages/web/src/store/sagas.ts
@@ -25,7 +25,6 @@ import signOnSaga from 'common/store/pages/signon/sagas'
 import trackSagas from 'common/store/pages/track/sagas'
 import trendingPageSagas from 'common/store/pages/trending/sagas'
 import playerSagas from 'common/store/player/sagas'
-import mobileQueueSagas from 'common/store/queue/mobileSagas'
 import queueSagas from 'common/store/queue/sagas'
 import reachabilitySagas from 'common/store/reachability/sagas'
 import recoveryEmailSagas from 'common/store/recovery-email/sagas'
@@ -86,7 +85,7 @@ const NATIVE_MOBILE = process.env.REACT_APP_NATIVE_MOBILE
 
 export default function* rootSaga() {
   yield fork(setupBackend)
-  let sagas = ([] as (() => Generator<any, void, any>)[]).concat(
+  const sagas = ([] as (() => Generator<any, void, any>)[]).concat(
     // TODO(nkang): Move to common/store/pages/search-page/sagas once migrated
     searchPageTracksSagas(),
 
@@ -193,7 +192,6 @@ export default function* rootSaga() {
   )
   if (NATIVE_MOBILE) {
     sagas.push(initInterface)
-    sagas = sagas.concat(mobileQueueSagas())
   }
   yield all(sagas.map(fork))
 }
