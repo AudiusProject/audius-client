401d0abf8ef2d39dc7afe3dab5f2df2d9e1b9430
sliptype <sliptype@gmail.com>
2022-09-01 15:58:39 -0500

    Still wait for account

--- packages/web/src/components/search-bar/store/sagas.ts
+++ packages/web/src/components/search-bar/store/sagas.ts
@@ -1,4 +1,9 @@
-import { Name, accountSelectors, getContext } from '@audius/common'
+import {
+  Name,
+  accountSelectors,
+  getContext,
+  waitForAccount
+} from '@audius/common'
 import { call, cancel, fork, put, race, select, take } from 'typed-redux-saga'
 
 import { make } from 'common/store/analytics/actions'
@@ -13,6 +18,7 @@ const NATIVE_MOBILE = process.env.REACT_APP_NATIVE_MOBILE
 
 export function* getSearchResults(searchText: string) {
   const apiClient = yield* getContext('apiClient')
+  yield* waitForAccount()
   const userId = yield* select(getUserId)
 
   const results = yield* call([apiClient, 'getSearchAutocomplete'], {
