696e301eeea66950b1541a2364e20c1452e6a784
Raymond Jacobson <ray@audius.co>
2022-08-31 17:51:40 -0700

    Remove lifecycle

--- packages/mobile/src/store/lifecycle/selectors.ts
+++
@@ -1,5 +0,0 @@
-import type { AppState } from 'app/store'
-
-const getBaseState = (state: AppState) => state.lifecycle
-
-export const getLocation = (state: AppState) => getBaseState(state).location
