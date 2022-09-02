7c8c20f02e6ce9a02b3f55a11fd03c209736d907
Dylan Jeffers <dylan@audius.co>
2022-09-01 17:24:56 -0700

    Split up sign-out saga to web and native (#1847)

--- packages/mobile/src/store/sagas.ts
+++ packages/mobile/src/store/sagas.ts
@@ -28,7 +28,6 @@ import playerSagas from 'common/store/player/sagas'
 import profileSagas from 'common/store/profile/sagas'
 import queueSagas from 'common/store/queue/sagas'
 import searchBarSagas from 'common/store/search-bar/sagas'
-import signOutSagas from 'common/store/sign-out/sagas'
 import smartCollectionPageSagas from 'common/store/smart-collection/sagas'
 import socialSagas from 'common/store/social/sagas'
 import favoritePageSagas from 'common/store/user-list/favorites/sagas'
@@ -44,6 +43,8 @@ import { all, fork } from 'typed-redux-saga'
 import initKeyboardEvents from './keyboard/sagas'
 import notificationsSagasNative from './notifications/sagas'
 import oauthSagas from './oauth/sagas'
+import settingsSagas from './settings/sagas'
+import signOutSagas from './sign-out/sagas'
 
 export default function* rootSaga() {
   yield* fork(setupBackend)
@@ -93,6 +94,8 @@ export default function* rootSaga() {
     ...repostPageSagas(),
     ...supportingPageSagas(),
     ...topSupportersPageSagas(),
+    ...settingsSagas(),
+    ...signOutSagas(),
 
     // Application
     ...smartCollectionPageSagas(),
