1527a5c019d097a4d885f01dbcb7e8fe9c785b6f
sliptype <sliptype@gmail.com>
2022-09-02 15:40:42 -0500

    [C-924] Add fully native explore screen (#1819)

--- packages/mobile/src/services/env.ts
+++ packages/mobile/src/services/env.ts
@@ -1,3 +1,6 @@
+import Config from 'react-native-config'
+
 export const env = {
-  EAGER_DISCOVERY_NODES: process.env.REACT_APP_EAGER_DISCOVERY_NODES
+  EAGER_DISCOVERY_NODES: Config.EAGER_DISCOVERY_NODES,
+  EXPLORE_CONTENT_URL: Config.EXPLORE_CONTENT_URL
 }
