34f39c4ff1e406a334aba4284772a5a2a7c4b5da
Dylan Jeffers <dylan@audius.co>
2022-09-08 13:44:27 -0700

    Fix types

--- packages/common/src/services/env.ts
+++ packages/common/src/services/env.ts
@@ -1,5 +1,7 @@
+export type Environment = 'development' | 'staging' | 'production'
+
 export type Env = {
   EAGER_DISCOVERY_NODES?: string
   EXPLORE_CONTENT_URL?: string
-  ENVIRONMENT?: 'development' | 'staging' | 'production'
+  ENVIRONMENT?: Environment
 }
--- packages/web/src/services/env.ts
+++ packages/web/src/services/env.ts
@@ -1,5 +1,7 @@
+import { Environment } from '@audius/common'
+
 export const env = {
   EAGER_DISCOVERY_NODES: process.env.REACT_APP_EAGER_DISCOVERY_NODES,
   EXPLORE_CONTENT_URL: process.env.REACT_APP_EXPLORE_CONTENT_URL,
-  ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT
+  ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT as Environment
 }
