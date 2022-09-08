a9fe0815b0030b8855a98e9f81c503e81053ab80
Steve Perkins <stereosteve@gmail.com>
2022-09-08 14:43:46 -0400

    dms: encrypt message for receiver

--- packages/web/src/dms/wip_dms.ts
+++ packages/web/src/dms/wip_dms.ts
@@ -2,17 +2,33 @@
 /// basic client
 ///
 
+import { base64 } from '@scure/base'
 import { ChantCodec } from './codec'
 
 const baseUrl = `https://discoveryprovider3.staging.audius.co`
 
 export async function dmSend(toWallet: string, message: string) {
+  const toPublicKey = await getPublicKey(toWallet)
+  if (!toPublicKey) {
+    console.error(`failed to get public key for wallet ${toWallet}`)
+    return
+  }
+
+  /// this object will be encrypted and only visible to the reciever
+  /// the fields can be arbitrary, but they won't be visible to discovery postgres or anything
+  const innerMessage = {
+    message,
+    sentAt: new Date()
+  }
+
+  const encryptedMessage = await asymEncrypt(innerMessage, toPublicKey)
+
   const signed = await signRpc({
     id: new Date(),
     method: 'dm.send',
     params: {
       toWallet,
-      message
+      encryptedMessage
     }
   })
 
@@ -43,23 +59,52 @@ export async function dmGet() {
 
   const respBody = await resp.json()
 
+  const codec = getCodec()
   for (let rpc of respBody) {
-    console.log('dms', {
-      from: rpc.wallet,
-      to: rpc.params.toWallet,
-      message: rpc.params.message
-    })
+    if (rpc.params.encryptedMessage) {
+      const decrypted = await codec.decode(rpc.params.encryptedMessage.data)
+      if (decrypted) {
+        console.log('dms encrypted message from:', rpc.wallet, decrypted.data)
+      }
+    } else {
+      console.log('dms plain message (legacy)', {
+        from: rpc.wallet,
+        ...rpc.params
+      })
+    }
   }
 }
 
+async function getPublicKey(wallet: string): Promise<Uint8Array | undefined> {
+  const got = await fetch(baseUrl + `/clusterizer/pubkey/${wallet}`)
+  if (got.status != 200) return
+  const txt = await got.text()
+  return base64.decode(txt)
+}
+
 async function signRpc(rpc: any) {
+  const codec = getCodec()
+
+  const signed = codec.encode(rpc)
+  return signed
+}
+
+async function asymEncrypt(obj: any, forPublicKey: Uint8Array) {
+  const codec = getCodec()
+  return codec.encode(obj, {
+    encPublicKey: forPublicKey
+  })
+}
+
+function getCodec() {
   const audiusLibs = window.audiusLibs
   const wallet = audiusLibs.Account.hedgehog.getWallet()
   const privateKey = wallet.privateKey
-
+  if (!privateKey) {
+    throw new Error(`can't create codec: hedgehog wallet private key missing`)
+  }
   const codec = new ChantCodec(privateKey)
-  const signed = codec.encode(rpc)
-  return signed
+  return codec
 }
 
 Object.assign(window, {
