a32b4942742e988b42a79c644a46b8b8fc6ef4e7
Marcus Pasell <rickyrombo@users.noreply.github.com>
2022-09-19 12:03:12 -0700

    Revert "Allow empty decimal place if decimal point exists"
    This reverts commit fe4e3a87e5fc8832d7b524a3357dec10a40f4e65.

--- packages/common/src/utils/formatUtil.ts
+++ packages/common/src/utils/formatUtil.ts
@@ -125,7 +125,7 @@ export const formatNumberCommas = (num: number | string) => {
   const parts = num.toString().split('.')
   return (
     parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',') +
-    (parts[1] !== undefined ? '.' + parts[1] : '')
+    (parts[1] ? '.' + parts[1] : '')
   )
 }
 
