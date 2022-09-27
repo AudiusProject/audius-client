388d3c0bddb477da495c2f0ec4c9a3617c461e2a
Dylan Jeffers <dylan@audius.co>
2022-09-27 11:43:15 -0700

    Remove things

--- packages/mobile/src/components/web-app-account-sync/WebAppAccountSync.tsx
+++ packages/mobile/src/components/web-app-account-sync/WebAppAccountSync.tsx
@@ -2,15 +2,12 @@ import { getErrorMessage } from '@audius/common'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import type { NativeSyntheticEvent } from 'react-native'
 import { View } from 'react-native'
-import Config from 'react-native-config'
 import StaticServer from 'react-native-static-server'
 import { WebView } from 'react-native-webview'
 import type { WebViewMessage } from 'react-native-webview/lib/WebViewTypes'
 import { useAsync } from 'react-use'
 
 import { ENTROPY_KEY } from 'app/store/account/sagas'
-
-// const OLD_WEB_APP_STATIC_SERVER_PORT = Config.OLD_WEB_APP_STATIC_SERVER_PORT
 
 const injected = `
 (function() {
