304c72041b9587085258577945511799e6388514
Nikki Kang <kangaroo233@gmail.com>
2022-09-29 14:39:35 -0400

    only check isinternetreachable

--- packages/mobile/src/utils/connectivity.ts
+++ packages/mobile/src/utils/connectivity.ts
@@ -9,12 +9,9 @@ import { dispatch } from './../store/store'
 export const checkConnectivity = (netInfo: NetInfoState | null) => {
   if (!netInfo) return true
 
-  const { isConnected, isInternetReachable, type } = netInfo
-  // Unknown type or null reachability means that netinfo hasn't been able to
-  // determine connection status. We may actually be connected despite
-  // `isConnected` being `false`
-  if (type === 'unknown' || isInternetReachable === null) return true
-  return isConnected && isInternetReachable
+  const { isInternetReachable } = netInfo
+
+  return isInternetReachable !== false
 }
 
 // Latest connectivity value
