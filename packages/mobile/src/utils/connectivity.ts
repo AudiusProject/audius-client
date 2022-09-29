01307e9210c586b2e59104529aae0ec073ef3bdf
amendelsohn <andrew.j.mendelsohn@gmail.com>
2022-09-29 16:44:57 -0400

    double check before declaring offline state

--- packages/mobile/src/utils/connectivity.ts
+++ packages/mobile/src/utils/connectivity.ts
@@ -4,28 +4,56 @@ import NetInfo from '@react-native-community/netinfo'
 import { debounce } from 'lodash'
 import { AppState } from 'react-native'
 
-import { dispatch } from './../store/store'
+import { env } from 'app/services/env'
+import { dispatch } from 'app/store'
+
+const REACHABILITY_URL = env.REACHABILITY_URL
 
 export const checkConnectivity = (netInfo: NetInfoState | null) => {
   if (!netInfo) return true
 
-  const { isConnected, isInternetReachable, type } = netInfo
-  // Unknown type or null reachability means that netinfo hasn't been able to
-  // determine connection status. We may actually be connected despite
-  // `isConnected` being `false`
-  if (type === 'unknown' || isInternetReachable === null) return true
-  return isConnected && isInternetReachable
+  const { isInternetReachable } = netInfo
+  return !!isInternetReachable
 }
 
 // Latest connectivity value
 export const Connectivity: { netInfo: NetInfoState | null } = { netInfo: null }
+// Check that a response from REACHABILITY_URL is valid
+const isResponseValid = (response: Response | undefined) =>
+  response && response.ok
 
-const updateConnectivity = (state: NetInfoState) => {
+const ping = async () => {
+  // If there's no reachability url available, consider ourselves reachable
+  if (!REACHABILITY_URL) {
+    console.warn('No reachability url provided')
+    return true
+  }
+
+  try {
+    const response = await fetch(REACHABILITY_URL, { method: 'GET' })
+
+    if (isResponseValid(response)) {
+      console.debug('Reachability call succeeded')
+      return true
+    }
+    console.debug('Reachability call failed')
+    return false
+  } catch {
+    console.debug('Reachability call failed')
+    return false
+  }
+}
+
+const updateConnectivity = async (state: NetInfoState) => {
   Connectivity.netInfo = state
   if (AppState.currentState !== 'active') return
   const newValue = checkConnectivity(state)
   if (!newValue) {
-    dispatch(reachabilityActions.setUnreachable())
+    // Perform our own reachability test to be extra sure we're offline
+    const reachable = await ping()
+    if (!reachable) {
+      dispatch(reachabilityActions.setUnreachable())
+    }
   } else {
     dispatch(reachabilityActions.setReachable())
   }
