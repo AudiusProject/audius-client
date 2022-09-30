e01a811822b5f8d341068d7cee9e00d39af50313
sliptype <sliptype@gmail.com>
2022-09-30 16:28:31 -0500

    Bump react-screens to 3.17.0 to avoid crash on android

--- packages/mobile/src/screens/profile-screen/CoverPhoto.tsx
+++ packages/mobile/src/screens/profile-screen/CoverPhoto.tsx
@@ -67,23 +67,16 @@ export const CoverPhoto = ({ scrollY }: { scrollY?: Animated.Value }) => {
         styles={{ root: styles.imageRoot, image: styles.image }}
         resizeMode={isDefaultImage ? 'repeat' : undefined}
       >
-        {/*
-          Disable blur on android because it causes a crash.
-          See https://github.com/software-mansion/react-native-screens/pull/1406
-          TODO: C-423 pull in new version of react screens when the fix is released
-        */}
-        {Platform.OS === 'ios' ? (
-          <AnimatedBlurView
-            blurType={'dark'}
-            blurAmount={100}
-            style={[
-              { ...StyleSheet.absoluteFillObject, zIndex: 2 },
-              scrollY
-                ? { opacity: interpolateBlurViewOpacity(scrollY) }
-                : undefined
-            ]}
-          />
-        ) : null}
+        <AnimatedBlurView
+          blurType={'dark'}
+          blurAmount={100}
+          style={[
+            { ...StyleSheet.absoluteFillObject, zIndex: 2 },
+            scrollY
+              ? { opacity: interpolateBlurViewOpacity(scrollY) }
+              : undefined
+          ]}
+        />
       </DynamicImage>
       {isArtist ? (
         <Animated.View
