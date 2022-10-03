ba6ed6f38a97d73d58c0386f2736d42d6ab85d35
Nikki Kang <kangaroo233@gmail.com>
2022-10-03 11:24:03 -0400

    add cypress prober videos to artifacts

--- packages/web/src/pages/sign-on/components/mobile/ProfilePage.tsx
+++ packages/web/src/pages/sign-on/components/mobile/ProfilePage.tsx
@@ -21,7 +21,7 @@ const GENERAL_ADMISSION = process.env.REACT_APP_GENERAL_ADMISSION || ''
 const messages = {
   header: 'Tell Us About Yourself So Others Can Find You'
 }
-
+console.log('remove this')
 type ProfilePageProps = {
   profileImage?: AccountImage
   twitterId: any
