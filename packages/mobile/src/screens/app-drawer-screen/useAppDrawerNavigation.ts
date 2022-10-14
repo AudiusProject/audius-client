cfccf610db5ef43db21e92d932a14023a5d0cc9c
Dylan Jeffers <dylan@audius.co>
2022-10-14 16:10:29 -0700

    [C-1341] Implement notifications bottom tab screen

--- packages/mobile/src/screens/app-drawer-screen/useAppDrawerNavigation.ts
+++ packages/mobile/src/screens/app-drawer-screen/useAppDrawerNavigation.ts
@@ -1,14 +1,44 @@
 import { useContext } from 'react'
 
+import { FeatureFlags } from '@audius/common'
+
 import { useNavigation } from 'app/hooks/useNavigation'
+import { useFeatureFlag } from 'app/hooks/useRemoteConfig'
 
 import type { AppTabScreenParamList } from '../app-screen'
 import type { ProfileTabScreenParamList } from '../app-screen/ProfileTabScreen'
 
 import { AppDrawerContext } from '.'
 
+// Temporary navigation hook for notification components.
+// When MOBILE_NAV_OVERHAUL is true, use the native-drawer-nav
+// Otherwise use normal nav
+export const useNotificationNavigation = () => {
+  const { drawerHelpers } = useContext(AppDrawerContext)
+  const { isEnabled: isNavOverhaulEnabled } = useFeatureFlag(
+    FeatureFlags.MOBILE_NAV_OVERHAUL
+  )
+
+  const navigationOptions = isNavOverhaulEnabled
+    ? undefined
+    : {
+        customNativeNavigation: drawerHelpers
+      }
+
+  const navigation = useNavigation<
+    AppTabScreenParamList & ProfileTabScreenParamList
+  >(navigationOptions)
+
+  if (isNavOverhaulEnabled) {
+    navigation.navigate = navigation.push
+  }
+
+  return navigation
+}
+
 export const useAppDrawerNavigation = () => {
   const { drawerHelpers } = useContext(AppDrawerContext)
+
   return useNavigation<AppTabScreenParamList & ProfileTabScreenParamList>({
     customNativeNavigation: drawerHelpers
   })
