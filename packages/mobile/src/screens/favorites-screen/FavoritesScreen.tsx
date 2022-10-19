17252a22da9d63ad7940ec5a6af0deee2c9b25e2
amendelsohn <andrew.j.mendelsohn@gmail.com>
2022-10-19 09:56:27 -0400

    comment unused import

--- packages/mobile/src/screens/favorites-screen/FavoritesScreen.tsx
+++ packages/mobile/src/screens/favorites-screen/FavoritesScreen.tsx
@@ -12,7 +12,7 @@ import { Screen, ScreenContent, ScreenHeader } from 'app/components/core'
 import { DownloadToggle } from 'app/components/offline-downloads/DownloadToggle'
 import { TopTabNavigator } from 'app/components/top-tab-bar'
 import { usePopToTopOnDrawerOpen } from 'app/hooks/usePopToTopOnDrawerOpen'
-import { useFeatureFlag } from 'app/hooks/useRemoteConfig'
+// import { useFeatureFlag } from 'app/hooks/useRemoteConfig'
 import { DOWNLOAD_REASON_FAVORITES } from 'app/services/offline-downloader'
 
 import { AlbumsTab } from './AlbumsTab'
