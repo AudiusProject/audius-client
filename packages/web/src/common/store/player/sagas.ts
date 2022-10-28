9bd83b30502a97a6b0074e8ab6bbc49a9fda766d
amendelsohn <andrew.j.mendelsohn@gmail.com>
2022-10-28 16:19:08 -0400

    type error

--- packages/web/src/common/store/player/sagas.ts
+++ packages/web/src/common/store/player/sagas.ts
@@ -125,7 +125,7 @@ export function* watchCollectiblePlay() {
       const audioPlayer = yield* getContext('audioPlayer')
       const endChannel = eventChannel((emitter) => {
         audioPlayer.load(
-          [],
+          '',
           () => {
             if (onEnd) {
               emitter(onEnd({}))
