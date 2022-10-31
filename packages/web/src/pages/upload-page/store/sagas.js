f26eb7ce3afa7096f3a5cd50afc24d9b38782131
Joseph Lee <joeylee0925@gmail.com>
2022-10-31 17:08:14 -0400

    Change the action channel dispatcher

--- packages/web/src/pages/upload-page/store/sagas.js
+++ packages/web/src/pages/upload-page/store/sagas.js
@@ -659,8 +659,8 @@ export function* handleUploads({
 
   console.debug('Finished upload')
 
-  yield cancel(actionDispatcherTask)
   yield call(progressChan.close)
+  yield cancel(actionDispatcherTask)
   return returnVal
 }
 
