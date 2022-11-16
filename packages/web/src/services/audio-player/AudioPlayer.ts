9b6936fa09d6418c85c4c028c0f31d3de3a17d7c
Sebastian Klingler <sliptype@gmail.com>
2022-11-16 16:50:55 -0600

    Actually pause audio when AudioPlayer pauses (#2277)

--- packages/web/src/services/audio-player/AudioPlayer.ts
+++ packages/web/src/services/audio-player/AudioPlayer.ts
@@ -381,9 +381,8 @@ export class AudioPlayer {
     if (this.audioCtx && IS_CHROME_LIKE) {
       // See comment above in the `play()` method.
       this.source!.disconnect()
-    } else {
-      this.audio.pause()
     }
+    this.audio.pause()
   }
 
   stop = () => {
