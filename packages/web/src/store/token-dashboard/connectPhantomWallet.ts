78aa87009f2c1d431681f84bfc833110ecf0be5a
Dylan Jeffers <dylan@audius.co>
2022-12-02 12:30:02 -0800

    Remove extra file

--- packages/web/src/store/token-dashboard/connectPhantomWallet.ts
+++
@@ -1,9 +0,0 @@
-export function* connectPhantomWallet() {
-  const connectingWallet = solana.publicKey?.toString()
-  const disconnect = async () => {
-    await solana.disconnect()
-  }
-  if (connectingWallet) {
-    yield* call(connectSPLWallet, connectingWallet, solana, disconnect)
-  }
-}
