32f414790fb3b663b6ec96a17c0ff2a671d8b03a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2022-12-07 09:58:44 -0800

    extra log inc pubkey

--- packages/web/src/services/audius-backend/BuyAudio.ts
+++ packages/web/src/services/audius-backend/BuyAudio.ts
@@ -163,6 +163,10 @@ export const pollForAudioBalanceChange = async ({
     )
     return tokenAccountInfo.amount
   }
+  console.debug(
+    'AUDIO balance polling exceeded maximum retries:',
+    tokenAccount.toString()
+  )
   throw new Error('AUDIO balance polling exceeded maximum retries')
 }
 
@@ -200,6 +204,10 @@ export const pollForSolBalanceChange = async ({
     )
     return balance
   }
+  console.debug(
+    'SOL balance polling exceeded maximum retries:',
+    rootAccount.toString()
+  )
   throw new Error('SOL balance polling exceeded maximum retries')
 }
 
