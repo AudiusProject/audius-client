bd68f2f0224838097eab605b4237a408fd8164ee
Michael Piazza <michael.piazza.mp@gmail.com>
2022-12-12 17:07:21 -0500

    Cache getLocation call

--- packages/web/src/utils/gdpr.ts
+++ packages/web/src/utils/gdpr.ts
@@ -1,14 +1,50 @@
 import { getLocation } from 'services/Location'
 
 const DISMISSED_COOKIE_BANNER_KEY = 'dismissCookieBanner'
+const IS_EU_KEY = 'isEU'
+const IS_EU_CACHE_TTL_MS = 7 * 24 * 3600 * 1000
 
-export const getIsInEU = async () => {
-  const location = await getLocation()
-  if (!location) return false
-  return location.in_eu
+/**
+ * Helper to get isEU from local storage with expiry
+ */
+const getCachedIsEU = () => {
+  const cachedIsEU = localStorage.getItem(IS_EU_KEY)
+  if (!cachedIsEU) {
+    console.debug('loc: no cached location')
+    return null
+  }
+  const { isEU, expiry }: { isEU: boolean; expiry: number } =
+    JSON.parse(cachedIsEU)
+
+  if (Date.now() > expiry) {
+    console.debug('loc: expired, removd')
+    localStorage.removeItem(IS_EU_KEY)
+    return null
+  }
+  return isEU
 }
 
-export const shouldShowCookieBanner = async (): Promise<boolean> => {
+export const getIsInEU = async () => {
+  const cachedIsEU = getCachedIsEU()
+  if (cachedIsEU !== null) {
+    console.debug('loc: found cached loc')
+    return cachedIsEU
+  }
+  const location = await getLocation()
+  if (!location) {
+    return false
+  }
+  const isEU = location.in_eu
+  localStorage.setItem(
+    IS_EU_KEY,
+    JSON.stringify({ isEU, expiry: Date.now() + IS_EU_CACHE_TTL_MS })
+  )
+  return isEU
+}
+
+export const shouldShowCookieBanner = async (): Promise<
+  boolean | undefined
+> => {
   if (
     process.env.NODE_ENV === 'production' &&
     process.env.REACT_APP_ENVIRONMENT === 'production'
@@ -18,7 +54,6 @@ export const shouldShowCookieBanner = async (): Promise<boolean> => {
     const isInEU = await getIsInEU()
     return isInEU
   }
-  return false
 }
 
 export const dismissCookieBanner = () => {
