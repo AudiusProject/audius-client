0e394d5ffb4613b215efe99c4079e83805763e95
amendelsohn <andrew.j.mendelsohn@gmail.com>
2022-12-14 12:17:52 -0500

    undo unneccessary change

--- packages/mobile/src/screens/favorites-screen/AlbumsTab.tsx
+++ packages/mobile/src/screens/favorites-screen/AlbumsTab.tsx
@@ -19,11 +19,10 @@ export const AlbumsTab = () => {
   const [filterValue, setFilterValue] = useState('')
 
   const userAlbums = useProxySelector(
-    (state: CommonState) => {
-      return getAccountCollections(state, filterValue).filter(
+    (state: CommonState) =>
+      getAccountCollections(state, filterValue).filter(
         (collection) => collection.is_album
-      )
-    },
+      ),
     [filterValue]
   )
 
