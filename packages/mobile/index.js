800e92609dd85978286c8e4a9fab42845782a535
Nikki Kang <kangaroo233@gmail.com>
2022-12-15 13:54:09 -0500

    move

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler'
+import allPromisesSettled from 'promise.allsettled'
 import { AppRegistry, LogBox } from 'react-native'
 import TrackPlayer from 'react-native-track-player'
 
@@ -11,6 +12,7 @@ require('node-libs-react-native/globals')
 // Needed for @solana/web3.js to run correctly
 require('react-native-get-random-values')
 require('react-native-url-polyfill/auto')
+allPromisesSettled.shim()
 
 const App = require('./src/App').default
 
