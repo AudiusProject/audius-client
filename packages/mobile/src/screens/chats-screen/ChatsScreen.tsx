207ad239d0101cacdd677a4d9f7b96ed2f37274a
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-01-04 02:49:33 -0600

    show more message info

--- packages/mobile/src/screens/chats-screen/ChatsScreen.tsx
+++ packages/mobile/src/screens/chats-screen/ChatsScreen.tsx
@@ -9,15 +9,31 @@ import { Screen, Text } from 'app/components/core'
 export const ChatsScreen = () => {
   const dispatch = useDispatch()
   const chats = useSelector(chatSelectors.getChats)
+  const id = 'clbyghuy800003bat0th2ivy5'
+  const messages =
+    useSelector((state) => chatSelectors.getChatMessages(state, id)) ?? []
 
   useEffect(() => {
     dispatch(chatActions.fetchMoreChats())
   }, [dispatch])
 
+  useEffect(() => {
+    dispatch(chatActions.fetchNewChatMessages({ chatId: id }))
+  }, [dispatch])
+
   return (
     <Screen>
       <View>
-        <Text>Hello world {chats?.length}</Text>
+        <Text>You have {chats?.length} chats:</Text>
+        {messages.map((message) => {
+          return (
+            <View key={message.message_id}>
+              <Text>{message.sender_user_id} says:</Text>
+              <Text>{message.message}</Text>
+              <Text>{message.created_at}</Text>
+            </View>
+          )
+        })}
       </View>
     </Screen>
   )
