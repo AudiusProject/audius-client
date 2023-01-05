2b66ed9fea35b88bc92ec38cc967ce1fc97d538b
Dylan Jeffers <dylan@audius.co>
2023-01-04 23:13:54 -0800

    Fix all usages

--- packages/common/src/store/pages/saved-page/types.ts
+++ packages/common/src/store/pages/saved-page/types.ts
@@ -6,13 +6,16 @@ import {
   Collection,
   Favorite,
   LineupState,
-  LineupTrack
+  LineupTrack,
+  Status
 } from '../../../models'
 
-export default interface SavesPageState {
+export interface SavedPageState {
   localSaves: { [id: number]: UID }
-  tracks: LineupState<{ id: ID; dateSaved: string }>
+  tracks: LineupState<LineupTrack & { id: ID; dateSaved: string }>
   saves: Favorite[]
+  hasReachedEnd: boolean
+  initialFetch: Status
 }
 
 export enum SavedPageTabs {
