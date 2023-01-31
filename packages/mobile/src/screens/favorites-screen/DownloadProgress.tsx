30ed2332bf4cecdef07c0f34d6e5e051159777f5
Raymond Jacobson <ray@audius.co>
2023-01-31 13:50:17 -0800

    Use successful downloads to keep track of progress bar

--- packages/mobile/src/screens/favorites-screen/DownloadProgress.tsx
+++ packages/mobile/src/screens/favorites-screen/DownloadProgress.tsx
@@ -35,6 +35,9 @@ export const DownloadProgress = () => {
       status === OfflineDownloadStatus.SUCCESS ||
       status === OfflineDownloadStatus.ERROR
   ).length
+  const numDownloadsSuccess = Object.values(downloadStatus).filter(
+    (status) => status === OfflineDownloadStatus.SUCCESS
+  ).length
 
   // Only render if there are active downloads
   if (numDownloadsComplete === numDownloads) return null
@@ -42,7 +45,7 @@ export const DownloadProgress = () => {
   return (
     <View style={styles.root}>
       <Text style={styles.text} color='neutral' weight='demiBold' fontSize='xs'>
-        {`${numDownloadsComplete}/${numDownloads}`}
+        {`${numDownloadsSuccess}/${numDownloads}`}
       </Text>
       <ProgressBar
         style={{
