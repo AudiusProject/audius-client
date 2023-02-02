e7411aec33c7e1b4f74c2639a5db9219814cc7db
Raymond Jacobson <ray@audius.co>
2023-02-01 19:23:30 -0800

    [C-2001] Remove queued downloads if deleted/unlisted/etc

--- packages/mobile/src/services/offline-downloader/workers/collectionDownloadWorker.ts
+++ packages/mobile/src/services/offline-downloader/workers/collectionDownloadWorker.ts
@@ -1,13 +1,41 @@
 import type { CancellablePromise } from 'react-native-job-queue'
-import { Worker } from 'react-native-job-queue'
+import queue, { Worker } from 'react-native-job-queue'
 
-import { downloadCollection } from '../offline-downloader'
+import { isEqualCollectionPayload } from 'app/services/offline-downloader/offline-download-queue'
+
+import {
+  downloadCollection,
+  DownloadCollectionError
+} from '../offline-downloader'
 import type { CollectionForDownload } from '../types'
 
 export const COLLECTION_DOWNLOAD_WORKER = 'collection_download_worker'
 export type CollectionDownloadWorkerPayload = CollectionForDownload
 
-const onFailure = ({ payload }: { payload: CollectionForDownload }) => {}
+const onFailure = async (
+  { payload }: { payload: CollectionForDownload },
+  error: Error
+) => {
+  switch (error.message) {
+    case DownloadCollectionError.IS_DELETED:
+    case DownloadCollectionError.IS_PRIVATE: {
+      const jobs = await queue.getJobs()
+      jobs.forEach((rawJob) => {
+        if (rawJob.workerName === COLLECTION_DOWNLOAD_WORKER) {
+          const parsedPayload: CollectionDownloadWorkerPayload = JSON.parse(
+            rawJob.payload
+          )
+          if (isEqualCollectionPayload(payload, parsedPayload)) {
+            queue.removeJob(rawJob)
+          }
+        }
+      })
+      break
+    }
+    default:
+      break
+  }
+}
 
 const executor = (payload: CollectionDownloadWorkerPayload) => {
   const promise: CancellablePromise<void> = downloadCollection(payload)
