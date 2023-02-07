2bd1c4b112b9c75487862c24144eb3c1e799eab1
Saliou Diallo <saliou@audius.co>
2023-02-07 09:44:13 -0500

    Do not fetch remixes as we poll for unlocked premium track signatures

--- packages/web/src/common/store/premiumContent/sagas.ts
+++ packages/web/src/common/store/premiumContent/sagas.ts
@@ -332,7 +332,9 @@ function* pollPremiumTrack({
       yield* put(updatePremiumTrackStatus({ status: 'UNLOCKED' }))
       break
     }
-    yield* put(trackPageActions.fetchTrack(null, slug, handle, false, true))
+    yield* put(
+      trackPageActions.fetchTrack(null, slug, handle, false, true, false)
+    )
     yield* delay(frequency)
   }
 }
