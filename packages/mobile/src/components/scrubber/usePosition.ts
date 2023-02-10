0faa3db4d5e7d974f301f97df2f69f2f64faaec1
Dylan Jeffers <dylan@audius.co>
2023-02-09 16:11:28 -0800

    [C-2040] Improve duration counter accuracy/performance

---
+++ packages/mobile/src/components/scrubber/usePosition.ts
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+import { formatSeconds } from '@audius/common'
+import type { TextInput } from 'react-native'
+
+export const usePosition = (
+  mediaKey: string,
+  duration: number,
+  isPlaying: boolean,
+  isInteracting: boolean
+) => {
+  const positionRef = useRef(0)
+  const positionElementRef = useRef<TextInput>(null)
+
+  const setPosition = useCallback((position: number) => {
+    positionRef.current = position
+    positionElementRef.current?.setNativeProps({
+      text: formatSeconds(positionRef.current)
+    })
+  }, [])
+
+  useEffect(() => {
+    let outerTimeout: NodeJS.Timeout
+    let currentTimeout: NodeJS.Timeout
+
+    const updatePosition = () => {
+      const timeout = setTimeout(() => {
+        positionRef.current += 1
+
+        positionElementRef.current?.setNativeProps({
+          text: formatSeconds(positionRef.current)
+        })
+
+        if (positionRef.current <= duration) {
+          currentTimeout = updatePosition()
+        }
+      }, 1000)
+
+      return timeout
+    }
+
+    if (isPlaying && !isInteracting) {
+      outerTimeout = updatePosition()
+    }
+
+    return () => {
+      clearTimeout(outerTimeout)
+      clearTimeout(currentTimeout)
+    }
+  }, [isPlaying, isInteracting, duration])
+
+  useEffect(() => {
+    setPosition(0)
+  }, [mediaKey, setPosition])
+
+  return { ref: positionElementRef, setPosition }
+}
