7e3411034a3083c1bf49075f009f3ebe4262a816
Dylan Jeffers <dylan@audius.co>
2023-02-10 23:41:56 -0800

    add batch size

--- packages/mobile/src/store/offline-downloads/sagas/redownloadStaleTracksSaga.ts
+++ packages/mobile/src/store/offline-downloads/sagas/redownloadStaleTracksSaga.ts
@@ -5,11 +5,13 @@ import { getOfflineTrackMetadata } from '../selectors'
 import type { OfflineItem } from '../slice'
 
 const STALE_DURATION_TRACKS = moment.duration(1, 'second')
+const STALE_BATCH_SIZE = 20
 
 export function* redownloadStaleTracksSaga() {
   const offlineTrackMetadata = yield* select(getOfflineTrackMetadata)
 
   const staleTrackIds = Object.keys(offlineTrackMetadata)
+    .slice(0, STALE_BATCH_SIZE)
     .map((id) => parseInt(id, 10))
     .filter((trackId) => {
       const metadata = offlineTrackMetadata[trackId]
