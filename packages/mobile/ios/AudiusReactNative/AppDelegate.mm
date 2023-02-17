6358fc3557c2814688b228bf5c6daae07f78124f
sliptype <sliptype@gmail.com>
2023-02-16 18:10:18 -0600

    Remove unneeded AppDelegate changes

--- packages/mobile/ios/AudiusReactNative/AppDelegate.mm
+++ packages/mobile/ios/AudiusReactNative/AppDelegate.mm
@@ -263,20 +263,4 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 
 #endif
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
-{
-    if ([[TikTokOpenSDKApplicationDelegate sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation]) {
-        return YES;
-    }
-    return NO;
-}
-
-- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
-{
-    if ([[TikTokOpenSDKApplicationDelegate sharedInstance] application:application openURL:url sourceApplication:nil annotation:nil]) {
-        return YES;
-    }
-    return NO;
-}
-
 @end
