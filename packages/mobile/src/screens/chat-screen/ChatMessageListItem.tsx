1c458745d379dff418f4ae0c4f110c499bc9869a
dharit-tan <dharit.tan@gmail.com>
2023-02-22 17:18:14 -0500

    fixed ChatTail

--- packages/mobile/src/screens/chat-screen/ChatMessageListItem.tsx
+++ packages/mobile/src/screens/chat-screen/ChatMessageListItem.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-
 import {
   accountSelectors,
   decodeHashId,
@@ -7,10 +5,9 @@ import {
 } from '@audius/common'
 import type { ChatMessage } from '@audius/sdk'
 import { View } from 'react-native'
-import Svg, { Path, Rect } from 'react-native-svg'
 import { useSelector } from 'react-redux'
 
-// import { ReactComponent as ChatTail } from 'app/assets/images/ChatTail.svg'
+import ChatTail from 'app/assets/images/ChatTail.svg'
 import { Text } from 'app/components/core'
 import { makeStyles } from 'app/styles'
 import { useThemePalette } from 'app/utils/theme'
@@ -21,27 +18,6 @@ const messages = {
   newMessage: 'New Message',
   s: 's'
 }
-
-const ChatTail = ({ fill }) => (
-  <Svg
-    width='25'
-    height='12'
-    viewBox='0 0 25 12'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <Path
-      d='M11 0V12H22.3944C24.3733 12 25.1503 9.43355 23.5038 8.3359L11 0Z'
-      fill={fill}
-    />
-    <Rect
-      width='11'
-      height='12'
-      transform='matrix(-1 0 0 1 11 0)'
-      fill={fill}
-    />
-  </Svg>
-)
 
 const useStyles = makeStyles(({ spacing, palette, typography }) => ({
   rootOtherUser: {
@@ -104,36 +80,17 @@ const useStyles = makeStyles(({ spacing, palette, typography }) => ({
     paddingVertical: spacing(1),
     borderRadius: spacing(0.5)
   },
-  // tailIsAuthor: {
-  //   display: 'flex',
-  //   position: 'absolute',
-  //   right: -7,
-  //   zIndex: -1,
-  //   bottom: 47,
-  //   width: 18,
-  //   height: 12
-  // },
-  // tailOtherUser: {
-  //   display: 'flex',
-  //   position: 'absolute',
-  //   left: -7,
-  //   zIndex: -1,
-  //   bottom: 47,
-  //   width: 18,
-  //   height: 12,
-  //   transform: [{ scaleX: -1 }]
-  // }
-  tailIsAuthor: {
+  tail: {
     display: 'flex',
     position: 'relative',
-    marginRight: -14,
-    marginTop: -12
+    marginTop: -12,
+    zIndex: -1
+  },
+  tailIsAuthor: {
+    marginRight: -12
   },
   tailOtherUser: {
-    display: 'flex',
-    position: 'relative',
-    marginLeft: -14,
-    marginTop: -12,
+    marginLeft: -12,
     transform: [{ scaleX: -1 }]
   }
 }))
@@ -168,7 +125,12 @@ export const ChatMessageListItem = ({
         </View>
         {hasTail && (
           <>
-            <View style={isAuthor ? styles.tailIsAuthor : styles.tailOtherUser}>
+            <View
+              style={[
+                styles.tail,
+                isAuthor ? styles.tailIsAuthor : styles.tailOtherUser
+              ]}
+            >
               <ChatTail fill={isAuthor ? palette.secondary : palette.white} />
             </View>
             <View style={styles.dateContainer}>
