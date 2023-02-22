f24079f84424e65edd97591fb540bf83f82bd981
Dylan Jeffers <dylan@audius.co>
2023-02-21 19:02:22 -0800

    Fix remove offline + analytics

--- packages/mobile/src/store/offline-downloads/sagas/watchRemoveOfflineItems.ts
+++ packages/mobile/src/store/offline-downloads/sagas/watchRemoveOfflineItems.ts
@@ -1,5 +1,5 @@
 import RNFetchBlob from 'rn-fetch-blob'
-import { takeEvery, select, call } from 'typed-redux-saga'
+import { takeEvery, select, call, all } from 'typed-redux-saga'
 
 import { make, track } from 'app/services/analytics'
 import {
@@ -12,7 +12,12 @@ import {
   getOfflineCollectionsStatus,
   getOfflineTrackStatus
 } from '../selectors'
-import type { RemoveOfflineEntriesAction } from '../slice'
+import type {
+  CollectionStatus,
+  OfflineEntry,
+  RemoveOfflineEntriesAction,
+  TrackStatus
+} from '../slice'
 import { removeOfflineItems } from '../slice'
 
 export function* watchRemoveOfflineItems() {
@@ -24,23 +29,36 @@ function* deleteItemsFromDisk(action: RemoveOfflineEntriesAction) {
   const trackStatus = yield* select(getOfflineTrackStatus)
   const collectionStatus = yield* select(getOfflineCollectionsStatus)
 
-  for (const item of items) {
+  yield* all(
+    items.map((item) =>
+      call(removeItemFromDisk, item, trackStatus, collectionStatus)
+    )
+  )
+}
+
+function* removeItemFromDisk(
+  item: OfflineEntry,
+  trackStatus: TrackStatus,
+  collectionStatus: CollectionStatus
+) {
+  if (item.type === 'collection' && !collectionStatus[item.id]) {
     track(make({ eventName: EventNames.OFFLINE_MODE_REMOVE_ITEM, ...item }))
-    if (item.type === 'collection' && !collectionStatus[item.id]) {
-      const collectionDirectory = yield* call(
-        getLocalCollectionDir,
-        item.id.toString()
-      )
-      const exists = yield* call(RNFetchBlob.fs.exists, collectionDirectory)
-      if (exists) {
-        yield* call(RNFetchBlob.fs.unlink, collectionDirectory)
-      }
-    } else if (item.type === 'track' && !trackStatus[item.id]) {
-      const trackDirectory = yield* call(getLocalTrackDir, item.id.toString())
-      const exists = yield* call(RNFetchBlob.fs.exists, trackDirectory)
-      if (exists) {
-        yield* call(RNFetchBlob.fs.unlink, trackDirectory)
-      }
+    const collectionDirectory = yield* call(
+      getLocalCollectionDir,
+      item.id.toString()
+    )
+    const exists = yield* call(RNFetchBlob.fs.exists, collectionDirectory)
+    if (exists) {
+      console.log('removing collection!')
+      yield* call(RNFetchBlob.fs.unlink, collectionDirectory)
+    }
+  } else if (item.type === 'track' && !trackStatus[item.id]) {
+    track(make({ eventName: EventNames.OFFLINE_MODE_REMOVE_ITEM, ...item }))
+    const trackDirectory = yield* call(getLocalTrackDir, item.id.toString())
+    const exists = yield* call(RNFetchBlob.fs.exists, trackDirectory)
+    if (exists) {
+      console.log('removing track!')
+      yield* call(RNFetchBlob.fs.unlink, trackDirectory)
     }
   }
 }
