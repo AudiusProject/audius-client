35ec538ebfbec8a555109fae5c0602e9bc0bd1a9
Dylan Jeffers <dylan@audius.co>
2023-02-22 20:49:55 -0800

    Remove logs and track

--- packages/mobile/src/store/offline-downloads/sagas/watchRemoveOfflineItems.ts
+++ packages/mobile/src/store/offline-downloads/sagas/watchRemoveOfflineItems.ts
@@ -1,12 +1,10 @@
 import RNFetchBlob from 'rn-fetch-blob'
 import { takeEvery, select, call, all } from 'typed-redux-saga'
 
-import { make, track } from 'app/services/analytics'
 import {
   getLocalCollectionDir,
   getLocalTrackDir
 } from 'app/services/offline-downloader'
-import { EventNames } from 'app/types/analytics'
 
 import {
   getOfflineCollectionsStatus,
@@ -42,22 +40,18 @@ function* removeItemFromDisk(
   collectionStatus: CollectionStatus
 ) {
   if (item.type === 'collection' && !collectionStatus[item.id]) {
-    track(make({ eventName: EventNames.OFFLINE_MODE_REMOVE_ITEM, ...item }))
     const collectionDirectory = yield* call(
       getLocalCollectionDir,
       item.id.toString()
     )
     const exists = yield* call(RNFetchBlob.fs.exists, collectionDirectory)
     if (exists) {
-      console.log('removing collection!')
       yield* call(RNFetchBlob.fs.unlink, collectionDirectory)
     }
   } else if (item.type === 'track' && !trackStatus[item.id]) {
-    track(make({ eventName: EventNames.OFFLINE_MODE_REMOVE_ITEM, ...item }))
     const trackDirectory = yield* call(getLocalTrackDir, item.id.toString())
     const exists = yield* call(RNFetchBlob.fs.exists, trackDirectory)
     if (exists) {
-      console.log('removing track!')
       yield* call(RNFetchBlob.fs.unlink, trackDirectory)
     }
   }
