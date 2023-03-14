fe0e3ea5748781aae91c7cee09405c7ff3e0880e
Nikki Kang <kangaroo233@gmail.com>
2023-03-14 15:53:50 +0100

    code review

--- packages/mobile/src/utils/appVersionWithCodepush.ts
+++ packages/mobile/src/utils/appVersionWithCodepush.ts
@@ -1,6 +1,7 @@
 import codePush from 'react-native-code-push'
 import VersionNumber from 'react-native-version-number'
 
+/** Note, this will be `null` if we are unable to get the CodePush update metadata for whatever reason. Thus, do not use this as the definitive app version value. */
 let versionInfo: string | null = null
 codePush
   .getUpdateMetadata()
