c15417211f3c1a80ddb3850521c948a20159b946
Raymond Jacobson <ray@audius.co>
2023-03-24 09:42:19 -0700

    Remove log

--- packages/web/src/common/store/profile/sagas.js
+++ packages/web/src/common/store/profile/sagas.js
@@ -124,7 +124,6 @@ export function* fetchOpenSeaAssets(user) {
   const { wallets } = yield apiClient.getAssociatedWallets({
     userID: user.user_id
   })
-  console.log('raymont', wallets)
   const collectiblesMap = yield call(fetchOpenSeaAssetsForWallets, [
     user.wallet,
     ...wallets
