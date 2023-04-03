bc139c46b3cfad180b7b0cd67afd12936749f328
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-04-03 16:25:59 -0500

    downloads migration saga

---
+++ packages/mobile/src/store/offline-downloads/sagas/migrateOfflineDataPathSaga.ts
@@ -0,0 +1,70 @@
+import path from 'path'
+
+import moment from 'moment'
+import RNFS from 'react-native-fs'
+import RNFetchBlob from 'rn-fetch-blob'
+import { call } from 'typed-redux-saga'
+
+import { downloadsRoot } from 'app/services/offline-downloader'
+
+import { getIsOfflineEnabled } from './getIsOfflineEnabled'
+
+// Copy util
+
+const {
+  fs: { dirs, unlink, exists }
+} = RNFetchBlob
+
+const legacyDownloadsRoot = path.join(dirs.DocumentDir, 'downloads')
+
+// Move downloads from legacy storage location to the updated path (currently: /Documents -> /Caches)
+export function* migrateOfflineDataPathSaga() {
+  const isOfflineModeEnabled = yield* call(getIsOfflineEnabled)
+  if (!isOfflineModeEnabled) return
+
+  const legacyFilesExist = yield* call(exists, legacyDownloadsRoot)
+  if (!legacyFilesExist) return // TODO: fire action?
+
+  const start = moment()
+  yield* call(copyRecursive, legacyDownloadsRoot, downloadsRoot)
+  yield* call(unlink, legacyDownloadsRoot)
+  const end = moment()
+  const duration = end.diff(start)
+  console.log('Migration duration', duration)
+}
+
+async function copyRecursive(source: string, destination: string) {
+  console.log(`${source} => ${destination}`)
+  // reads items from source directory
+  const items = await RNFS.readDir(source)
+
+  // creates destination directory
+  console.log(`Output directory: ${destination}`)
+  if (!(await RNFS.exists(destination))) {
+    await RNFS.mkdir(destination)
+  }
+
+  await Promise.all(
+    items.map(async (item) => {
+      //  item is a file
+      if (item.isFile() === true) {
+        console.log(`Input file: ${item.path}`)
+        const destinationFile = destination + '/' + item.name
+
+        // copies file
+        console.log(`Output file: ${destinationFile}`)
+        if (!(await exists(destinationFile))) {
+          await RNFS.moveFile(item.path, destinationFile)
+        }
+      } else {
+        // item is a directory
+        console.log(`Input directory: ${item.path}`)
+
+        const subDirectory = source + '/' + item.name
+        const subDestinationDirectory = destination + '/' + item.name
+
+        await copyRecursive(subDirectory, subDestinationDirectory)
+      }
+    })
+  )
+}
