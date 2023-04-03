92d5069f2ea447a47be30060ecdc58c4b8b96060
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-04-03 17:20:58 -0500

    comments; abort logic

--- packages/mobile/src/store/offline-downloads/sagas/migrateOfflineDataPathSaga.ts
+++ packages/mobile/src/store/offline-downloads/sagas/migrateOfflineDataPathSaga.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 
-import moment from 'moment'
 import RNFS from 'react-native-fs'
 import RNFetchBlob from 'rn-fetch-blob'
 import { call } from 'typed-redux-saga'
@@ -9,12 +8,12 @@ import { downloadsRoot } from 'app/services/offline-downloader'
 
 import { getIsOfflineEnabled } from './getIsOfflineEnabled'
 
-// Copy util
-
 const {
   fs: { dirs, unlink, exists }
 } = RNFetchBlob
 
+// Current migration: 4/3/2023
+// dirs.DocumentDir -> dirs.CacheDir
 const legacyDownloadsRoot = path.join(dirs.DocumentDir, 'downloads')
 
 // Move downloads from legacy storage location to the updated path (currently: /Documents -> /Caches)
@@ -25,16 +24,16 @@ export function* migrateOfflineDataPathSaga() {
   const legacyFilesExist = yield* call(exists, legacyDownloadsRoot)
   if (!legacyFilesExist) return // TODO: fire action?
 
-  const start = moment()
-  yield* call(copyRecursive, legacyDownloadsRoot, downloadsRoot)
-  yield* call(unlink, legacyDownloadsRoot)
-  const end = moment()
-  const duration = end.diff(start)
-  console.log('Migration duration', duration)
+  try {
+    yield* call(copyRecursive, legacyDownloadsRoot, downloadsRoot)
+  } finally {
+    // If we fail, nuke the legacy directory to ensure we don't retry the process on every startup
+    yield* call(unlink, legacyDownloadsRoot)
+  }
 }
 
+// Util to recursively copy a directory since neither RNFS or rn-fetch-blob come with one
 async function copyRecursive(source: string, destination: string) {
-  console.log(`${source} => ${destination}`)
   // reads items from source directory
   const items = await RNFS.readDir(source)
 
