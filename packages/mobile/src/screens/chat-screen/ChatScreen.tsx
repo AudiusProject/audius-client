ab8ac6dea47f9a3291ad040c308678a79f1cac9f
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-04-26 00:17:17 -0400

    [PAY-1106] Unmount reaction popup animations (#3271)

--- packages/mobile/src/screens/chat-screen/ChatScreen.tsx
+++ packages/mobile/src/screens/chat-screen/ChatScreen.tsx
@@ -279,7 +279,7 @@ export const ChatScreen = () => {
     )
   }
 
-  const closeReactionPopup = useCallback(() => {
+  const onCloseReactionPopup = useCallback(() => {
     setShouldShowPopup(false)
     dispatch(setReactionsPopupMessageId({ messageId: null }))
   }, [setShouldShowPopup, dispatch])
@@ -393,7 +393,8 @@ export const ChatScreen = () => {
               containerBottom={chatContainerBottom.current}
               isAuthor={decodeHashId(popupMessage?.sender_user_id) === userId}
               message={popupMessage}
-              closePopup={closeReactionPopup}
+              shouldShowPopup={shouldShowPopup}
+              onClose={onCloseReactionPopup}
             />
           ) : null}
         </Portal>
