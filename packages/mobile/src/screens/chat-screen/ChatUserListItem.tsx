790ae421cd4e263f89255a0e45e8ee09a3743711
Dharit Tantiviramanond <dharit.tan@gmail.com>
2023-04-27 09:55:18 -0400

    lint

--- packages/mobile/src/screens/chat-screen/ChatUserListItem.tsx
+++ packages/mobile/src/screens/chat-screen/ChatUserListItem.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 
 import { chatActions, chatSelectors } from '@audius/common'
 import type { User } from '@audius/common'
