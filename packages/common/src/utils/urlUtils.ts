6054e44d16e326d979cf6b058e046978f003e4a1
Dylan Jeffers <dylan@audius.co>
2023-05-03 12:37:59 -0700

    Include staging

--- packages/common/src/utils/urlUtils.ts
+++ packages/common/src/utils/urlUtils.ts
@@ -1,6 +1,6 @@
 const audiusUrlRegex =
   // eslint-disable-next-line no-useless-escape
-  /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?(audius.co)(\/.+)?/gim
+  /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?(audius.co|staging.audius.co)(\/.+)?/gim
 
 export const isAudiusUrl = (url: string) => new RegExp(audiusUrlRegex).test(url)
 export const getPathFromAudiusUrl = (url: string) =>
