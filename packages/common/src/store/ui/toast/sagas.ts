73163cf18f13e9d18f61c3f878bcc9bcaefcfba3
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-05-30 19:19:50 -0700

    Rename addToast to scheduleDismissal

--- packages/common/src/store/ui/toast/sagas.ts
+++ packages/common/src/store/ui/toast/sagas.ts
@@ -2,7 +2,7 @@ import { put, takeEvery, delay } from 'typed-redux-saga'
 
 import { uuid } from 'utils/uid'
 
-import { addToast, dismissToast, toast } from './slice'
+import { scheduleDismissal, dismissToast, toast } from './slice'
 import { ToastAction } from './types'
 
 const TOAST_TIMEOUT = 10000
@@ -10,7 +10,7 @@ const TOAST_TIMEOUT = 10000
 function* handleToast(action: ToastAction) {
   const { timeout = TOAST_TIMEOUT, ...toastConfig } = action.payload
   const toast = { ...toastConfig, key: uuid() }
-  yield* put(addToast(toast))
+  yield* put(scheduleDismissal(toast))
   yield delay(timeout)
   yield* put(dismissToast({ key: toast.key }))
 }
