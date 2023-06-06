fec3c0b819188025fd38672b51216c38212b05ed
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-06-06 16:41:45 -0400

    simpler implementation

--- packages/common/src/store/pages/chat/selectors.ts
+++ packages/common/src/store/pages/chat/selectors.ts
@@ -2,6 +2,7 @@ import { ChatPermission, UserChat } from '@audius/sdk'
 import { createSelector } from 'reselect'
 
 import { ID } from 'models/Identifiers'
+import { Status } from 'models/Status'
 import { accountSelectors } from 'store/account'
 import { cacheUsersSelectors } from 'store/cache'
 import { CommonState } from 'store/reducers'
@@ -28,8 +29,12 @@ export const getChatsStatus = (state: CommonState) =>
 export const getChatsSummary = (state: CommonState) =>
   state.pages.chat.chats.summary
 
-export const getHasMoreChats = (state: CommonState) =>
-  state.pages.chat.chats.hasMore
+export const getHasMoreChats = (state: CommonState) => {
+  const { status, summary } = state.pages.chat.chats
+  return (
+    status === Status.IDLE || (!!summary?.prev_count && summary.prev_count > 0)
+  )
+}
 
 export const getOptimisticReads = (state: CommonState) =>
   state.pages.chat.optimisticChatRead
--- packages/common/src/store/pages/chat/slice.ts
+++ packages/common/src/store/pages/chat/slice.ts
@@ -31,7 +31,6 @@ type ChatID = string
 type ChatState = {
   chats: EntityState<UserChatWithMessagesStatus> & {
     status: Status
-    hasMore: boolean
     summary?: TypedCommsResponse<UserChat>['summary']
   }
   messages: Record<
@@ -109,7 +108,6 @@ const recalculatePreviousMessageHasTail = (
 
 const initialState: ChatState = {
   chats: {
-    hasMore: true,
     status: Status.IDLE,
     ...chatsAdapter.getInitialState()
   },
@@ -160,11 +158,8 @@ const slice = createSlice({
       state,
       action: PayloadAction<TypedCommsResponse<UserChat[]>>
     ) => {
-      const { summary } = action.payload
       state.chats.status = Status.SUCCESS
-      state.chats.summary = summary
-      state.chats.hasMore =
-        summary?.prev_count !== undefined && summary.prev_count > 0
+      state.chats.summary = action.payload.summary
       for (const chat of action.payload.data) {
         if (!(chat.chat_id in state.messages)) {
           state.messages[chat.chat_id] = chatMessagesAdapter.getInitialState()
