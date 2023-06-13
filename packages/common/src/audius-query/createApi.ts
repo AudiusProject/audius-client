f25c2acf2944ad4ad42a12a1cbe5d6881d27f821
Dylan Jeffers <dylan@audius.co>
2023-06-13 09:51:05 -0700

    [C-2761] Add mutations to audius-query

--- packages/common/src/audius-query/createApi.ts
+++ packages/common/src/audius-query/createApi.ts
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 
 import { CaseReducerActions, createSlice } from '@reduxjs/toolkit'
+import { produce } from 'immer'
 import { isEqual, mapValues } from 'lodash'
 import { denormalize, normalize } from 'normalizr'
 import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
 
 import { useProxySelector } from 'hooks/useProxySelector'
 import { Kind } from 'models/Kind'
@@ -67,6 +69,21 @@ export const createApi = <
   }
 
   api.reducer = slice.reducer
+  api.util = {
+    updateQueryData:
+      (endpointName, fetchArgs, updateRecipe) =>
+      (dispatch: Dispatch, getState: () => any) => {
+        const key = getKeyFromFetchArgs(fetchArgs)
+        const endpointState = getState().api[reducerPath][endpointName][key]
+        if (!endpointState) return
+        const newState = produce(endpointState.nonNormalizedData, updateRecipe)
+        const updateAction =
+          slice.actions[`fetch${capitalize(endpointName as string)}Succeeded`]
+        if (updateAction) {
+          dispatch(updateAction({ fetchArgs, nonNormalizedData: newState }))
+        }
+      }
+  }
 
   return api
 }
@@ -235,6 +252,7 @@ const buildEndpointHooks = <
     )
 
     const context = useContext(AudiusQueryContext)
+
     useEffect(() => {
       if (isInitialValue) {
         dispatch(
@@ -259,34 +277,46 @@ const buildEndpointHooks = <
               fetchArgs
             }) as FetchLoadingAction
           )
+
+          endpoint.onQueryStarted?.(fetchArgs, { dispatch })
+
           const apiData = await endpoint.fetch(fetchArgs, context)
           if (!apiData) {
             throw new Error('Remote data not found')
           }
 
-          const { entities, result } = normalize(
-            { [endpoint.options.schemaKey]: apiData },
-            apiResponseSchema
-          )
-          dispatch(addEntries(Object.keys(entities), entities))
+          let nonNormalizedData: any
+          if (endpoint.options.schemaKey) {
+            const { entities, result } = normalize(
+              { [endpoint.options.schemaKey]: apiData },
+              apiResponseSchema
+            )
+            nonNormalizedData = result
+            dispatch(addEntries(Object.keys(entities), entities))
+          } else {
+            nonNormalizedData = apiData
+          }
 
           dispatch(
             // @ts-ignore
             actions[`fetch${capitalize(endpointName)}Succeeded`]({
               fetchArgs,
-              nonNormalizedData: result
+              nonNormalizedData
             }) as FetchSucceededAction
           )
+
+          endpoint.onQuerySuccess?.(nonNormalizedData, fetchArgs, { dispatch })
         } catch (e) {
           dispatch(
             // @ts-ignore
-            actions[`fetch${capitalize(endpointName)}Error`]({
+            actions({
               fetchArgs,
               errorMessage: getErrorMessage(e)
             }) as FetchErrorAction
           )
         }
       }
+
       fetchWrapped()
     }, [
       fetchArgs,
@@ -304,5 +334,185 @@ const buildEndpointHooks = <
 
     return { data: cachedData, status, errorMessage }
   }
-  api.hooks[`use${capitalize(endpointName)}`] = useQuery
+
+  // Hook to be returned as use<EndpointName>
+  const useMutation = (
+    hookOptions?: QueryHookOptions
+  ): [
+    (fetchArgs: Args, hookOptions?: QueryHookOptions) => void,
+    QueryHookResults<Data>
+  ] => {
+    const dispatch = useDispatch()
+    const [fetchArgs, setFetchArgs] = useState({})
+    const key = getKeyFromFetchArgs(fetchArgs)
+    const queryState = useSelector((state: CommonState) => {
+      if (!state.api[reducerPath]) {
+        throw new Error(
+          `State for ${reducerPath} is undefined - did you forget to register the reducer in @audius/common/src/api/reducers.ts?`
+        )
+      }
+      const endpointState: PerEndpointState<any> =
+        state.api[reducerPath][endpointName]
+
+      // Retrieve data from cache if lookup args provided
+      if (!endpointState[key]) {
+        if (
+          !(
+            endpoint.options?.idArgKey ||
+            endpoint.options?.idListArgKey ||
+            endpoint.options?.permalinkArgKey
+          ) ||
+          !endpoint.options?.kind ||
+          !endpoint.options?.schemaKey
+        )
+          return null
+        const { kind, idArgKey, idListArgKey, permalinkArgKey, schemaKey } =
+          endpoint.options
+
+        let cachedData = null
+        if (idArgKey && fetchArgs[idArgKey]) {
+          const idAsNumber =
+            typeof fetchArgs[idArgKey] === 'number'
+              ? fetchArgs[idArgKey]
+              : parseInt(fetchArgs[idArgKey])
+          cachedData = cacheSelectors.getEntry(state, {
+            kind,
+            id: idAsNumber
+          })
+        } else if (permalinkArgKey && fetchArgs[permalinkArgKey]) {
+          if (kind === Kind.TRACKS) {
+            cachedData = getTrack(state, {
+              permalink: fetchArgs[permalinkArgKey]
+            })
+          } else if (kind === Kind.COLLECTIONS) {
+            cachedData = getCollection(state, {
+              permalink: fetchArgs[permalinkArgKey]
+            })
+          }
+        } else if (idListArgKey && fetchArgs[idListArgKey]) {
+          const idsAsNumbers: number[] = fetchArgs[idListArgKey].map(
+            (id: string | number) =>
+              typeof id === 'number' ? id : parseInt(id)
+          )
+          const allEntities = mapValues(
+            cacheSelectors.getCache(state, { kind }).entries,
+            'metadata'
+          )
+          const entityHits = idsAsNumbers
+            .map((id) => allEntities[id])
+            .filter(removeNullable)
+          if (entityHits.length === idsAsNumbers.length) {
+            cachedData = entityHits
+          }
+        }
+
+        // cache hit
+        if (cachedData) {
+          const { result } = normalize(
+            { [schemaKey]: cachedData },
+            apiResponseSchema
+          )
+          return {
+            nonNormalizedData: result,
+            status: Status.SUCCESS,
+            isInitialValue: true,
+            errorMessage: undefined
+          }
+        }
+      }
+
+      return { ...endpointState[key] }
+    }, isEqual)
+
+    const { nonNormalizedData, status, errorMessage } = queryState ?? {
+      nonNormalizedData: null,
+      status: Status.IDLE
+    }
+
+    // Rehydrate local nonNormalizedData using entities from global normalized cache
+    let cachedData: Data = useProxySelector(
+      (state: CommonState) => {
+        if (hookOptions?.shallow && !endpoint.options.kind)
+          return nonNormalizedData
+        const entityMap = selectCommonEntityMap(
+          state,
+          endpoint.options.kind,
+          hookOptions?.shallow
+        )
+        return denormalize(nonNormalizedData, apiResponseSchema, entityMap)
+      },
+      [nonNormalizedData, apiResponseSchema, endpoint.options.kind]
+    )
+
+    const context = useContext(AudiusQueryContext)
+
+    const fetchWrapped = useCallback(
+      async (fetchArgs: any) => {
+        if (!context) return
+        if ([Status.LOADING, Status.ERROR, Status.SUCCESS].includes(status))
+          return
+        if (hookOptions?.disabled) return
+
+        try {
+          dispatch(
+            // @ts-ignore
+            actions[`fetch${capitalize(endpointName)}Loading`]({
+              fetchArgs
+            }) as FetchLoadingAction
+          )
+
+          endpoint.onQueryStarted?.(fetchArgs, { dispatch })
+
+          const apiData = await endpoint.fetch(fetchArgs, context)
+          if (!apiData) {
+            throw new Error('Remote data not found')
+          }
+
+          let nonNormalizedData: any
+          if (endpoint.options.schemaKey) {
+            const { entities, result } = normalize(
+              { [endpoint.options.schemaKey]: apiData },
+              apiResponseSchema
+            )
+            nonNormalizedData = result
+            dispatch(addEntries(Object.keys(entities), entities))
+          } else {
+            nonNormalizedData = apiData
+          }
+
+          dispatch(
+            // @ts-ignore
+            actions[`fetch${capitalize(endpointName)}Succeeded`]({
+              fetchArgs,
+              nonNormalizedData
+            }) as FetchSucceededAction
+          )
+
+          endpoint.onQuerySuccess?.(nonNormalizedData, fetchArgs, { dispatch })
+        } catch (e) {
+          dispatch(
+            // @ts-ignore
+            actions({
+              fetchArgs,
+              errorMessage: getErrorMessage(e)
+            }) as FetchErrorAction
+          )
+        }
+        setFetchArgs(fetchArgs)
+      },
+      [dispatch, status, context, hookOptions?.disabled]
+    )
+
+    if (endpoint.options?.schemaKey) {
+      cachedData = cachedData?.[endpoint.options?.schemaKey]
+    }
+
+    return [fetchWrapped, { data: cachedData, status, errorMessage }]
+  }
+
+  if (endpoint.options.type === 'mutation') {
+    api.hooks[`use${capitalize(endpointName)}`] = useMutation
+  } else {
+    api.hooks[`use${capitalize(endpointName)}`] = useQuery
+  }
 }
