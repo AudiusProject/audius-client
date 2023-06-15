0c4cd597d174705540a01b5babeaeeea3da7aaf2
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-15 10:56:57 -0700

    Rename to ChatPageProvider

--- packages/web/src/pages/chat-page/ChatPageProvider.tsx
+++ packages/web/src/pages/chat-page/ChatPageProvider.tsx
@@ -5,7 +5,9 @@ import { useIsMobile } from 'utils/clientUtil'
 import { ChatPage as DesktopChatPage } from './components/desktop/ChatPage'
 import { ChatPage as MobileChatPage } from './components/mobile/ChatPage'
 
-export const ChatPage = ({ match }: RouteComponentProps<{ id?: string }>) => {
+export const ChatPageProvider = ({
+  match
+}: RouteComponentProps<{ id?: string }>) => {
   const currentChatId = match.params.id
   const isMobile = useIsMobile()
   if (isMobile) {
