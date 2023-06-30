736b9c1a031df83d6d7d023bb1f73d43f98aa1ec
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-30 17:57:47 -0500

    Add cached DN back to DN selector, fixes [d]

--- packages/web/src/services/audius-sdk/discoveryNodeSelector.ts
+++ packages/web/src/services/audius-sdk/discoveryNodeSelector.ts
@@ -3,7 +3,64 @@ import { DiscoveryNodeSelectorService } from '@audius/common'
 import { env } from '../env'
 import { remoteConfigInstance } from '../remote-config/remote-config-instance'
 
+const DISCOVERY_PROVIDER_TIMESTAMP = '@audius/libs:discovery-node-timestamp'
+const CACHE_TTL = 24 * 60 * 1000 // 24 hours
+
+type CachedDiscoveryNodeTimestamp =
+  | {
+      endpoint?: string
+      timestamp?: number
+    }
+  | null
+  | undefined
+
+const cached = localStorage.getItem(DISCOVERY_PROVIDER_TIMESTAMP)
+let cachedDiscoveryNode
+if (cached) {
+  try {
+    const cachedDiscoveryNodeTimestamp = JSON.parse(
+      cached
+    ) as CachedDiscoveryNodeTimestamp
+    if (
+      cachedDiscoveryNodeTimestamp &&
+      cachedDiscoveryNodeTimestamp.timestamp &&
+      cachedDiscoveryNodeTimestamp.timestamp > new Date().getTime() - CACHE_TTL
+    ) {
+      cachedDiscoveryNode = cachedDiscoveryNodeTimestamp.endpoint ?? undefined
+      console.debug(
+        '[discovery-node-selector-service] Using cached discovery node',
+        cachedDiscoveryNode
+      )
+    } else {
+      console.debug(
+        '[discovery-node-selector-service] Cached discovery node expired',
+        cachedDiscoveryNodeTimestamp
+      )
+    }
+  } catch (e) {
+    console.error(
+      "[discovery-node-selector-service] Couldn't parse cached discovery node",
+      e
+    )
+  }
+}
+
 export const discoveryNodeSelectorService = new DiscoveryNodeSelectorService({
   env,
-  remoteConfigInstance
+  remoteConfigInstance,
+  initialSelectedNode: cachedDiscoveryNode,
+  onChange: (endpoint) => {
+    const newTimestamp: CachedDiscoveryNodeTimestamp = {
+      endpoint,
+      timestamp: new Date().getTime()
+    }
+    console.debug(
+      '[discovery-node-selector-service] Updating cached discovery provider',
+      newTimestamp
+    )
+    localStorage.setItem(
+      DISCOVERY_PROVIDER_TIMESTAMP,
+      JSON.stringify(newTimestamp)
+    )
+  }
 })
