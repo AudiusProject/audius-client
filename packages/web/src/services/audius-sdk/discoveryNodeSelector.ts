ec944c27e3c5a7c7ce0f6197b043d43ed0eb50c5
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-06-30 18:00:30 -0500

    cleanup

--- packages/web/src/services/audius-sdk/discoveryNodeSelector.ts
+++ packages/web/src/services/audius-sdk/discoveryNodeSelector.ts
@@ -14,53 +14,59 @@ type CachedDiscoveryNodeTimestamp =
   | null
   | undefined
 
-const cached = localStorage.getItem(DISCOVERY_PROVIDER_TIMESTAMP)
-let cachedDiscoveryNode
-if (cached) {
-  try {
-    const cachedDiscoveryNodeTimestamp = JSON.parse(
-      cached
-    ) as CachedDiscoveryNodeTimestamp
-    if (
-      cachedDiscoveryNodeTimestamp &&
-      cachedDiscoveryNodeTimestamp.timestamp &&
-      cachedDiscoveryNodeTimestamp.timestamp > new Date().getTime() - CACHE_TTL
-    ) {
-      cachedDiscoveryNode = cachedDiscoveryNodeTimestamp.endpoint ?? undefined
-      console.debug(
-        '[discovery-node-selector-service] Using cached discovery node',
-        cachedDiscoveryNode
-      )
-    } else {
-      console.debug(
-        '[discovery-node-selector-service] Cached discovery node expired',
-        cachedDiscoveryNodeTimestamp
+const getCachedDiscoveryNode = () => {
+  const cached = localStorage.getItem(DISCOVERY_PROVIDER_TIMESTAMP)
+  if (cached) {
+    try {
+      const cachedDiscoveryNodeTimestamp = JSON.parse(
+        cached
+      ) as CachedDiscoveryNodeTimestamp
+      if (
+        cachedDiscoveryNodeTimestamp &&
+        cachedDiscoveryNodeTimestamp.timestamp &&
+        cachedDiscoveryNodeTimestamp.timestamp >
+          new Date().getTime() - CACHE_TTL
+      ) {
+        const cachedDiscoveryNode =
+          cachedDiscoveryNodeTimestamp.endpoint ?? undefined
+        console.debug(
+          '[discovery-node-selector-service] Using cached discovery node',
+          cachedDiscoveryNode
+        )
+        return cachedDiscoveryNode
+      } else {
+        console.debug(
+          '[discovery-node-selector-service] Cached discovery node expired',
+          cachedDiscoveryNodeTimestamp
+        )
+      }
+    } catch (e) {
+      console.error(
+        "[discovery-node-selector-service] Couldn't parse cached discovery node",
+        e
       )
     }
-  } catch (e) {
-    console.error(
-      "[discovery-node-selector-service] Couldn't parse cached discovery node",
-      e
-    )
   }
+}
+
+const updateCachedDiscoveryNode = (endpoint: string) => {
+  const newTimestamp: CachedDiscoveryNodeTimestamp = {
+    endpoint,
+    timestamp: new Date().getTime()
+  }
+  console.debug(
+    '[discovery-node-selector-service] Updating cached discovery provider',
+    newTimestamp
+  )
+  localStorage.setItem(
+    DISCOVERY_PROVIDER_TIMESTAMP,
+    JSON.stringify(newTimestamp)
+  )
 }
 
 export const discoveryNodeSelectorService = new DiscoveryNodeSelectorService({
   env,
   remoteConfigInstance,
-  initialSelectedNode: cachedDiscoveryNode,
-  onChange: (endpoint) => {
-    const newTimestamp: CachedDiscoveryNodeTimestamp = {
-      endpoint,
-      timestamp: new Date().getTime()
-    }
-    console.debug(
-      '[discovery-node-selector-service] Updating cached discovery provider',
-      newTimestamp
-    )
-    localStorage.setItem(
-      DISCOVERY_PROVIDER_TIMESTAMP,
-      JSON.stringify(newTimestamp)
-    )
-  }
+  initialSelectedNode: getCachedDiscoveryNode(),
+  onChange: updateCachedDiscoveryNode
 })
