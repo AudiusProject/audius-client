923d114784dbfbeae91923737c501d9fa503a15f
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-07-06 17:55:16 -0400

    use custom property to avoid fighting with postcss

--- packages/stems/rollup.config.js
+++ packages/stems/rollup.config.js
@@ -40,8 +40,7 @@ export default {
             'src/assets/styles/sizes.css',
             'src/assets/styles/animations.css',
             'src/assets/styles/shadows.css',
-            'src/assets/styles/transforms.css',
-            'src/components/Button/Button.module.css'
+            'src/assets/styles/transforms.css'
           ]
         }),
         postcssCustomProperties({
