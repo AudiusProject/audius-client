a241db83df7acf6c7b5c037653a8031c3599bf61
Reed <3893871+dharit-tan@users.noreply.github.com>
2023-07-12 00:18:51 -0400

    Remove gated content feature flags from mobile (#3726)

--- packages/mobile/src/screens/edit-track-screen/fields/TrackVisibilityField.tsx
+++ packages/mobile/src/screens/edit-track-screen/fields/TrackVisibilityField.tsx
@@ -11,10 +11,8 @@ import { useField } from 'formik'
 
 import type { ContextualSubmenuProps } from 'app/components/core'
 import { ContextualSubmenu } from 'app/components/core'
-import { useIsGatedContentEnabled } from 'app/hooks/useIsGatedContentEnabled'
 
 const messages = {
-  trackVisibility: 'Track Visibility',
   availability: 'Availability',
   public: 'Public',
   collectibleGated: 'Collectible Gated',
@@ -68,18 +66,10 @@ export const TrackVisibilityField = (props: TrackVisibilityFieldProps) => {
     return [messages.public]
   }, [premiumConditions, isUnlisted, fieldVisibilityLabels])
 
-  const isGatedContentEnabled = useIsGatedContentEnabled()
-  const label = isGatedContentEnabled
-    ? messages.availability
-    : messages.trackVisibility
-  const submenuScreenName = isGatedContentEnabled
-    ? 'Availability'
-    : 'TrackVisibility'
-
   return (
     <ContextualSubmenu
-      label={label}
-      submenuScreenName={submenuScreenName}
+      label={messages.availability}
+      submenuScreenName={messages.availability}
       value={trackAvailabilityLabels}
       {...props}
     />
