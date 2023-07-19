10a907da06b1d07e6343e4794884ed97ee3cc2d0
Raymond Jacobson <ray@audius.co>
2023-07-19 16:42:16 -0700

    Add fallback

--- packages/embed/src/util/getArtworkUrl.js
+++ packages/embed/src/util/getArtworkUrl.js
@@ -1,8 +1,17 @@
+const fallback = 'https://creatornode.audius.co/content'
+
 export const getArtworkUrl = (
   collectionOrTrack,
   useDefaultArtworkIfMissing
 ) => {
-  const artworkUrl = (collectionOrTrack?.artwork || {})._480x480
+  console.log(collectionOrTrack)
+  let artworkUrl
+  if (collectionOrTrack?.artwork) {
+    artworkUrl = collectionOrTrack?.artwork._480x480
+  } else {
+    artworkUrl = `${fallback}/${collectionOrTrack?.coverArtSizes}/480x480.jpg`
+  }
+  console.log({ artworkUrl })
   if (!artworkUrl) {
     if (useDefaultArtworkIfMissing) {
       return 'https://download.audius.co/static-resources/preview-image.jpg'
