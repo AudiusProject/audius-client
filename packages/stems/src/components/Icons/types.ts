0b73c19f9d54b6df90e70512fc96d722f90dd8bb
Randy Schott <1815175+schottra@users.noreply.github.com>
2023-07-25 11:56:45 -0400

    update type for icons

--- packages/stems/src/components/Icons/types.ts
+++ packages/stems/src/components/Icons/types.ts
@@ -1,3 +1,3 @@
 import React from 'react'
 
-export type IconComponent = React.ComponentType<React.SVGProps<SVGElement>>
+export type IconComponent = React.ComponentType<React.SVGProps<SVGSVGElement>>
