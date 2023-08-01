b6aa91829ae0ecc13c9a70797e7fec1d680e7bea
Dylan Jeffers <dylan@audius.co>
2023-08-01 14:34:02 -0700

    Finalize release date field

---
+++ packages/web/src/components/form-fields/ContextualMenu.tsx
@@ -0,0 +1,196 @@
+import { ReactNode, ReactElement, useCallback } from 'react'
+
+import {
+  Button,
+  ButtonType,
+  IconCaretRight,
+  IconComponent,
+  Modal,
+  ModalContent,
+  ModalContentText,
+  ModalFooter,
+  ModalHeader,
+  ModalTitle
+} from '@audius/stems'
+import {
+  Form,
+  Formik,
+  FormikConfig,
+  FormikHelpers,
+  FormikValues,
+  useFormikContext
+} from 'formik'
+import { useToggle } from 'react-use'
+
+import { Icon } from 'components/Icon'
+import { HelperText } from 'components/data-entry/HelperText'
+import { Tile } from 'components/tile'
+import { Text } from 'components/typography'
+
+import styles from './ContextualMenu.module.css'
+
+const messages = {
+  save: 'Save'
+}
+
+type MenuFormProps = {
+  isOpen: boolean
+  onClose: () => void
+  label: string
+  icon: ReactNode
+  description: string
+  menuForm: ReactNode
+}
+
+const MenuForm = (props: MenuFormProps) => {
+  const { isOpen, onClose, label, icon, description, menuForm } = props
+  const { resetForm } = useFormikContext()
+
+  const handleCancel = useCallback(() => {
+    resetForm()
+    onClose()
+  }, [resetForm, onClose])
+
+  return (
+    <Modal onClose={handleCancel} isOpen={isOpen} size='medium'>
+      <ModalHeader>
+        <ModalTitle title={label} icon={icon} />
+      </ModalHeader>
+      <Form>
+        <ModalContent>
+          <ModalContentText>{description}</ModalContentText>
+          {menuForm}
+        </ModalContent>
+        <ModalFooter>
+          <Button
+            type={ButtonType.PRIMARY}
+            text={messages.save}
+            buttonType='submit'
+          />
+        </ModalFooter>
+      </Form>
+    </Modal>
+  )
+}
+
+type SelectedValueProps = {
+  label?: string
+  icon?: IconComponent
+  children?: ReactNode
+}
+
+export const SelectedValue = (props: SelectedValueProps) => {
+  const { label, icon, children } = props
+  return (
+    <span className={styles.selectedValue}>
+      {icon ? <Icon icon={icon} size='small' /> : null}
+      {label ? (
+        <Text variant='label' size='large'>
+          {label}
+        </Text>
+      ) : null}
+      {children}
+    </span>
+  )
+}
+
+type ContextualMenuProps<Value, FormValues extends FormikValues> = {
+  label: string
+  description: string
+  icon: ReactElement
+  renderValue?: (value: Value) => JSX.Element | null
+  menuForm: ReactNode
+  value: Value
+  error?: boolean
+  errorMessage?: string
+} & FormikConfig<FormValues>
+
+export const ContextualMenu = <
+  Value,
+  FormValues extends FormikValues = FormikValues
+>(
+  props: ContextualMenuProps<Value, FormValues>
+) => {
+  const {
+    label,
+    description,
+    icon,
+    menuForm,
+    renderValue: renderValueProp,
+    onSubmit,
+    value,
+    error,
+    errorMessage,
+    ...formikProps
+  } = props
+  const [isSubMenuOpen, toggleSubMenu] = useToggle(false)
+
+  const defaultRenderValue = useCallback((value: Value) => {
+    const hasValue = !value
+      ? false
+      : typeof value === 'string'
+      ? value
+      : Array.isArray(value)
+      ? value.length !== 0
+      : false
+
+    const values =
+      typeof value === 'string' ? [value] : Array.isArray(value) ? value : null
+
+    if (!hasValue || !values) return null
+
+    return (
+      <div className={styles.value}>
+        {values?.map((value) => (
+          <SelectedValue key={value} label={value} />
+        ))}
+      </div>
+    )
+  }, [])
+
+  const renderValue = renderValueProp ?? defaultRenderValue
+
+  const preview = (
+    <Tile onClick={toggleSubMenu} className={styles.root} elevation='flat'>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <div>
+            <Text className={styles.title} variant='title' size='large'>
+              {label}
+            </Text>
+          </div>
+          <div>
+            <Icon icon={IconCaretRight} />
+          </div>
+        </div>
+        <Text>{description}</Text>
+      </div>
+      {renderValue(value)}
+      {error ? <HelperText error>{errorMessage}</HelperText> : null}
+    </Tile>
+  )
+
+  const handleSubmit = useCallback(
+    (values: FormValues, helpers: FormikHelpers<FormValues>) => {
+      onSubmit(values, helpers)
+      toggleSubMenu()
+    },
+    [onSubmit, toggleSubMenu]
+  )
+
+  return (
+    <>
+      {preview}
+      <Formik {...formikProps} onSubmit={handleSubmit} enableReinitialize>
+        <MenuForm
+          label={label}
+          icon={icon}
+          description={description}
+          isOpen={isSubMenuOpen}
+          onClose={toggleSubMenu}
+          menuForm={menuForm}
+        />
+      </Formik>
+    </>
+  )
+}
---
+++ packages/web/src/pages/upload-page/fields/ReleaseDateField.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useMemo } from 'react'
+
+import { IconCalendar } from '@audius/stems'
+import cn from 'classnames'
+import { useField } from 'formik'
+import moment from 'moment'
+
+import { ContextualMenu, SelectedValue } from 'components/form-fields'
+import { Text } from 'components/typography'
+
+import { EditFormValues } from '../components/EditPageNew'
+
+import { DatePickerField } from './DatePickerField'
+import styles from './ReleaseDateField.module.css'
+const messages = {
+  title: 'Release Date',
+  description:
+    'Specify a release date (in the past) for your music. Release date will affect the order of content on your profile and is visible to users.'
+}
+
+const RELEASE_DATE = 'releaseDate'
+
+export type ReleaseDateFormValues = {
+  [RELEASE_DATE]: moment.Moment
+}
+
+type ReleaseDateValue = EditFormValues[typeof RELEASE_DATE]
+
+export const ReleaseDateField = () => {
+  const [{ value }, , { setValue }] = useField<ReleaseDateValue>(RELEASE_DATE)
+
+  const initialValues = useMemo(() => ({ [RELEASE_DATE]: value }), [value])
+
+  const onSubmit = useCallback(
+    (values: ReleaseDateFormValues) => {
+      setValue(values[RELEASE_DATE])
+    },
+    [setValue]
+  )
+
+  const renderValue = useCallback((value: ReleaseDateValue) => {
+    return (
+      <SelectedValue
+        label={moment(value).calendar().split(' at')[0]}
+        icon={IconCalendar}
+      >
+        <input
+          className={styles.input}
+          name={RELEASE_DATE}
+          value={moment(value).format('L')}
+          aria-readonly
+          readOnly
+        />
+      </SelectedValue>
+    )
+  }, [])
+
+  return (
+    <ContextualMenu
+      value={value}
+      label={messages.title}
+      description={messages.description}
+      icon={<IconCalendar className={styles.titleIcon} />}
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      menuForm={
+        <>
+          <Text
+            className={cn(styles.title, styles.modalHeading)}
+            variant='title'
+            size='large'
+          >
+            {messages.title}
+          </Text>
+          <Text>{messages.description}</Text>
+          <div className={styles.datePicker}>
+            <DatePickerField name={RELEASE_DATE} label={messages.title} />
+          </div>
+        </>
+      }
+      renderValue={renderValue}
+    />
+  )
+}
