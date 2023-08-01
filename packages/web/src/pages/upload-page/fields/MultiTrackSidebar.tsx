e6c452c83c3aa6c02d19381f4dc2094a36f8688f
amendelsohn <andrew.j.mendelsohn@gmail.com>
2023-08-01 17:20:24 -0500

    basic layout complete

--- packages/web/src/pages/upload-page/fields/MultiTrackSidebar.tsx
+++ packages/web/src/pages/upload-page/fields/MultiTrackSidebar.tsx
@@ -1,10 +1,19 @@
-import { HarmonyButton } from '@audius/stems'
+import { imageBlank as placeholderArt } from '@audius/common'
+import { HarmonyButton, HarmonyButtonType, IconUpload } from '@audius/stems'
 import cn from 'classnames'
-import { useField } from 'formik'
 
 import layoutStyles from 'components/layout/layout.module.css'
+import { Text } from 'components/typography'
 
 import { TrackForUpload } from '../components/types'
+import { useIndexedField } from '../forms/utils'
+
+import styles from './MultiTrackSidebar.module.css'
+
+const messages = {
+  title: 'UPLOADED TRACKS',
+  complete: 'Complete Upload'
+}
 
 type MultiTrackSidebarProps = {
   tracks: TrackForUpload[]
@@ -12,26 +21,66 @@ type MultiTrackSidebarProps = {
 
 export const MultiTrackSidebar = (props: MultiTrackSidebarProps) => {
   const { tracks } = props
-  const limit = tracks.length
-  const [{ value: index }, , { setValue: setIndex }] = useField(
-    'trackMetadatasIndex'
-  )
 
   return (
-    <div className={cn(layoutStyles.col, layoutStyles.gap2)}>
-      Track {index + 1} of {limit}
-      <div className={cn(layoutStyles.row, layoutStyles.gap2)}>
-        <HarmonyButton
-          text={'Prev'}
-          onClick={() => setIndex(Math.max(index - 1, 0))}
-          type='button'
-        />
-        <HarmonyButton
-          text={'Next'}
-          onClick={() => setIndex(Math.min(index + 1, limit - 1))}
-          type='button'
+    <div className={styles.root}>
+      <div className={cn(layoutStyles.col)}>
+        <div className={styles.title}>
+          <Text variant='title' size='xSmall'>
+            {messages.title}
+          </Text>
+        </div>
+        <div className={cn(styles.body, layoutStyles.col, layoutStyles.gap2)}>
+          <TrackNavigator tracks={tracks} />
+          <div className={styles.completeButton}>
+            <HarmonyButton
+              text={messages.complete}
+              variant={HarmonyButtonType.PRIMARY}
+              iconRight={IconUpload}
+              fullWidth
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const TrackNavigator = (props: MultiTrackSidebarProps) => {
+  const { tracks } = props
+  return (
+    <div className={cn(styles.tracks, layoutStyles.col)}>
+      {tracks.map((track, i) => (
+        <TrackRow key={i} track={track} index={i} />
+      ))}
+    </div>
+  )
+}
+
+type TrackRowProps = {
+  track: TrackForUpload
+  index: number
+}
+
+const TrackRow = (props: TrackRowProps) => {
+  const { track, index } = props
+  const [{ value: artworkUrl }] = useIndexedField<string>(
+    `trackMetadatas`,
+    index,
+    'artwork.url'
+  )
+  return (
+    <div className={cn(styles.track, layoutStyles.row, layoutStyles.gap3)}>
+      <div className={layoutStyles.row}>
+        <Text className={styles.trackIndex}>{index + 1}</Text>
+        <div
+          className={styles.artwork}
+          style={{
+            backgroundImage: `url(${artworkUrl || placeholderArt})`
+          }}
         />
       </div>
+      {track.metadata.title}
     </div>
   )
 }
