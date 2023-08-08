b4fd83e0c76883c8c7b2911030cf8094e018406b
Marcus Pasell <3690498+rickyrombo@users.noreply.github.com>
2023-08-08 14:43:54 -0700

    Add LeavingAudiusDrawer to mobile

---
+++ packages/mobile/src/components/leaving-audius-drawer/LeavingAudiusDrawer.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import { useLeavingAudiusModal } from '@audius/common'
+import { View } from 'react-native'
+
+import IconExternalLink from 'app/assets/images/iconExternalLink.svg'
+import IconInfo from 'app/assets/images/iconInfo.svg'
+import Drawer from 'app/components/drawer/Drawer'
+import { makeStyles } from 'app/styles'
+
+import { Button, Text, useLink } from '../core'
+import { HelpCallout } from '../help-callout/HelpCallout'
+
+const messages = {
+  title: 'Are You Sure?',
+  content: 'This link is taking you to the following website',
+  visit: 'Visit Site',
+  back: 'Go Back'
+}
+
+const useStyles = makeStyles(({ spacing }) => ({
+  root: {
+    gap: spacing(4),
+    paddingBottom: spacing(6),
+    paddingHorizontal: spacing(4)
+  },
+  button: {
+    width: '100%'
+  }
+}))
+
+export const LeavingAudiusDrawer = () => {
+  const styles = useStyles()
+  const [{ isOpen, link }, , close] = useLeavingAudiusModal()
+  const { onPress: onLinkPress } = useLink(link)
+  return (
+    <Drawer
+      isOpen={isOpen}
+      onClose={close}
+      title={messages.title}
+      titleIcon={IconInfo}
+    >
+      <View style={styles.root}>
+        <Text>{messages.content}</Text>
+        <HelpCallout content={link} icon={IconExternalLink} />
+        <Button
+          style={styles.button}
+          title={messages.visit}
+          onPress={onLinkPress}
+        />
+        <Button
+          style={styles.button}
+          variant={'common'}
+          title={messages.back}
+          onPress={close}
+        />
+      </View>
+    </Drawer>
+  )
+}
