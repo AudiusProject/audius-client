89b597253db2f5029e2ddefa02bea7bfda69c46c
Dylan Jeffers <dylan@audius.co>
2023-08-17 17:48:15 -0700

    Add Avatar component

--- packages/web/src/hooks/useUserProfilePicture.ts
+++ packages/web/src/hooks/useUserProfilePicture.ts
@@ -3,11 +3,15 @@ import {
   SquareSizes,
   useImageSize,
   cacheUsersActions,
-  imageProfilePicEmpty as profilePicEmpty
+  imageProfilePicEmpty as profilePicEmpty,
+  cacheUsersSelectors
 } from '@audius/common'
 import { useDispatch } from 'react-redux'
 
+import { useSelector } from 'utils/reducer'
+
 const { fetchProfilePicture } = cacheUsersActions
+const { getUser } = cacheUsersSelectors
 
 export const useUserProfilePicture = (
   userId: number | null,
@@ -21,6 +25,27 @@ export const useUserProfilePicture = (
     dispatch,
     id: userId,
     sizes: profilePictureSizes,
+    size,
+    action: fetchProfilePicture,
+    defaultImage,
+    load
+  })
+}
+
+export const useUserProfilePicture2 = (
+  userId: number | null,
+  size: SquareSizes,
+  defaultImage: string = profilePicEmpty as string,
+  load = true
+) => {
+  const dispatch = useDispatch()
+  const profilePictureSizes = useSelector(
+    (state) => getUser(state, { id: userId })?._profile_picture_sizes
+  )
+  return useImageSize({
+    dispatch,
+    id: userId,
+    sizes: profilePictureSizes ?? null,
     size,
     action: fetchProfilePicture,
     defaultImage,
