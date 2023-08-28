84bd2817bb64464bae91c622497f77745a1659a1
sliptype <sliptype@gmail.com>
2023-08-28 18:47:56 -0500

    Try using global sentry-cli

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -2,6 +2,4 @@ export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
 export EXTRA_PACKAGER_ARGS="--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map"
 
-
-cp ../node_modules/@sentry/cli/bin/sentry-cli .
-sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
+/usr/local/bin/sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
