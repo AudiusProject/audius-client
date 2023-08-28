b7896ab95b5b4af98d5a6ca8c174b98504d4381f
sliptype <sliptype@gmail.com>
2023-08-28 18:18:13 -0500

    Copy sentry cli

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -2,4 +2,6 @@ export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
 export EXTRA_PACKAGER_ARGS="--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map"
 
-../node_modules/@sentry/cli/bin/sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
+
+cp ../node_modules/@sentry/cli/bin/sentry-cli .
+sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
