75de75ed0af47469a585d6f33338c271c09900d5
sliptype <sliptype@gmail.com>
2023-08-28 10:53:06 -0500

    Locate node and sentry binaries differently

--- packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
+++ packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
@@ -1,5 +1,31 @@
 export SENTRY_PROPERTIES=sentry.properties
-if [[ -z "${SENTRY_BINARY}" ]]; then
-  export SENTRY_BINARY=../../../node_modules/@sentry/cli/bin/sentry-cli
+
+# Setup nvm and set node
+[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+
+if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+. "$HOME/.nvm/nvm.sh"
+elif [[ -x "$(command -v brew)" && -s "$(brew --prefix nvm)/nvm.sh" ]]; then
+. "$(brew --prefix nvm)/nvm.sh"
 fi
-$SENTRY_BINARY upload-dsym
+
+# Set up the nodenv node version manager if present
+if [[ -x "$HOME/.nodenv/bin/nodenv" ]]; then
+eval "$("$HOME/.nodenv/bin/nodenv" init -)"
+fi
+
+# Set up the fnm node version manager if present
+if [[ -s "$HOME/.fnm/fnm" ]]; then
+  eval "`$HOME/.fnm/fnm env --multi`"
+fi
+
+# Trying notion
+if [ -z "$NODE_BINARY" ]; then
+if [[ -s "$HOME/.notion/bin/node" ]]; then
+export NODE_BINARY="$HOME/.notion/bin/node"
+fi
+fi
+
+[ -z "$NODE_BINARY" ] && export NODE_BINARY="node"
+
+$NODE_BINARY ../../../node_modules/@sentry/cli/bin/sentry-cli upload-dsym
