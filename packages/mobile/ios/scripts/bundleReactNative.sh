508c02e9c970ea261abdaec7493a12579184fd2f
sliptype <sliptype@gmail.com>
2023-08-28 17:49:07 -0500

    Fix paths

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -2,4 +2,4 @@ export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
 export EXTRA_PACKAGER_ARGS="--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map"
 
-../../../node_modules/@sentry/cli/bin/sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
+../node_modules/@sentry/cli/bin/sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
--- packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
+++ packages/mobile/ios/scripts/uploadSentryDebugSymbols.sh
@@ -1,4 +1,4 @@
 export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
 
-../../../node_modules/@sentry/cli/bin/sentry-cli upload-dsym
+../node_modules/@sentry/cli/bin/sentry-cli upload-dsym
