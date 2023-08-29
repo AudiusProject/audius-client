4e5d864a621b311d7ed9f1ef820ff92e55c7427b
sliptype <sliptype@gmail.com>
2023-08-29 09:10:23 -0500

    Escape quotes

--- packages/mobile/ios/scripts/bundleReactNative.sh
+++ packages/mobile/ios/scripts/bundleReactNative.sh
@@ -1,5 +1,5 @@
 export NODE_BINARY=node
 export SENTRY_PROPERTIES=sentry.properties
-export EXTRA_PACKAGER_ARGS="--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map"
+export EXTRA_PACKAGER_ARGS=\"--sourcemap-output $DERIVED_FILE_DIR/main.jsbundle.map\"
 
 /usr/local/bin/sentry-cli react-native xcode ../node_modules/react-native/scripts/react-native-xcode.sh
