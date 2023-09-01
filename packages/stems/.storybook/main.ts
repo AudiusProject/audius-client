f4437ae837350aff06d6502d5e342b1b3e43aaf1
sliptype <sliptype@gmail.com>
2023-09-01 14:56:17 -0500

    Update storybook

--- packages/stems/.storybook/main.ts
+++ packages/stems/.storybook/main.ts
@@ -1,4 +1,51 @@
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y']
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config: any) => {
+    config.module.rules.find(
+      (rule) => rule.test.toString() === '/\\.css$/'
+    ).exclude = /\.module\.css$/
+
+    config.module.rules = config.module.rules.filter(
+      (rule) => rule.test && !rule.test.test('.svg')
+    )
+
+    config.module.rules = [
+      {
+        test: /\.svg$/,
+        use: [require.resolve('@svgr/webpack')]
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              }
+            }
+          }
+        ]
+      },
+      ...config.module.rules
+    ]
+
+    config.resolve.plugins = [new TsconfigPathsPlugin()]
+
+    return config
+  }
 }
