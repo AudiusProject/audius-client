736726c076640413c51a8651cf620bf9dce38596
Dylan Jeffers <dylan@audius.co>
2023-09-05 09:55:27 -0700

    Fix lint

--- packages/web/src/pages/upload-page/fields/availability/collectible-gated/CollectibleGatedRadioField.tsx
+++ packages/web/src/pages/upload-page/fields/availability/collectible-gated/CollectibleGatedRadioField.tsx
@@ -57,7 +57,6 @@ export const CollectibleGatedRadioField = (
       label={messages.collectibleGated}
       value={TrackAvailabilityType.COLLECTIBLE_GATED}
       disabled={disabled}
-      hintText={disabled ? messages.noCollectibles : undefined}
       description={
         <CollectibleGatedDescription
           hasCollectibles={hasCollectibles}
